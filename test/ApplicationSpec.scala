9705e6b072462757a53f361d9330ec6327b86c18
Mike Bryant <mikesname@gmail.com>
2012-10-10 19:19:38 +0100

    Fix ApplicationSpec tests, which were using the wrong port config and thus only working through chance.

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -19,33 +19,31 @@ import com.typesafe.config.ConfigFactory
 class ApplicationSpec extends Specification {
   sequential
 
-  // Load the test server using a different config...
-  val config = ConfigFactory.load("test.conf")
-  val port = config.getInt("neo4j.server.port")
-  val endpoint = config.getString("neo4j.server.endpoint")
+  val testPort = 7575
+  val config = Map("neo4j.server.port" -> testPort)
 
-  val runner: ServerRunner = new ServerRunner(classOf[ApplicationSpec].getName, port)
+  val runner: ServerRunner = new ServerRunner(classOf[ApplicationSpec].getName, testPort)
   runner.getConfigurator
     .getThirdpartyJaxRsClasses()
-    .add(new ThirdPartyJaxRsPackage(classOf[EhriNeo4jFramedResource[_]].getPackage.getName, "/" + endpoint));
+    .add(new ThirdPartyJaxRsPackage(classOf[EhriNeo4jFramedResource[_]].getPackage.getName, "/ehri"));
   runner.start();
 
   "Application" should {
     "send 404 on a bad request" in {
-      running(FakeApplication()) {
+      running(FakeApplication(additionalConfiguration=config)) {
         route(FakeRequest(GET, "/boum")) must beNone
       }
     }
 
     "list should get json" in {
-      running(FakeApplication()) {
+      running(FakeApplication(additionalConfiguration=config)) {
         val list = route(FakeRequest(GET, "/documentaryUnit/list")).get
         status(list) must equalTo(OK)
       }
     }
 
     "show should get a wierd string" in {
-      running(FakeApplication()) {
+      running(FakeApplication(additionalConfiguration=config)) {
         // FIXME: Set the the unit that is world-accessible because we
         // can't yet log in from tests...
         val show = route(FakeRequest(GET, "/documentaryUnit/show/7")).get
@@ -55,7 +53,7 @@ class ApplicationSpec extends Specification {
     }
 
     "render the index page" in {
-      running(FakeApplication()) {
+      running(FakeApplication(additionalConfiguration=config)) {
         val home = route(FakeRequest(GET, "/")).get
 
         status(home) must equalTo(OK)
