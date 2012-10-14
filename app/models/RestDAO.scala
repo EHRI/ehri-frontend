aff798587c97987f2d6340747e0540932e494490
Mike Bryant <mikesname@gmail.com>
2012-10-14 19:41:39 +0100

    Distinguish between different types of bad request.

--- app/models/RestDAO.scala
+++ app/models/RestDAO.scala
@@ -5,14 +5,17 @@ import scala.concurrent.Future
 import play.api.libs.ws.{ WS, Response }
 import play.api.libs.json.{ JsArray, JsValue }
 import play.api.Play
-
 import com.codahale.jerkson.Json.generate
+import com.codahale.jerkson.Json.generate
+import play.api.libs.json.JsObject
+import play.api.libs.json.JsString
 
 sealed trait RestError
 case object PermissionDenied extends RestError
 case object ValidationError extends RestError
+case object DeserializationError extends RestError
+case object IntegrityError extends RestError
 case object ItemNotFound extends RestError
-import com.codahale.jerkson.Json.generate
 
 trait RestDAO {
 
@@ -33,11 +36,15 @@ trait RestDAO {
   }
 
   protected def checkError(response: Response): Either[RestError, Response] = {
-    //println("RESPONSE: " + response.body)
     response.status match {
       case OK | CREATED => Right(response)
       case UNAUTHORIZED => Left(PermissionDenied)
-      case BAD_REQUEST => Left(ValidationError)
+      case BAD_REQUEST => (response.json \ "error") match {
+        case JsString("ValidationError") => Left(ValidationError)
+        case JsString("DeserializationError") => Left(DeserializationError)
+        case JsString("IntegrityError") => Left(IntegrityError)
+        case _ => throw sys.error("Unexpected response: %d: %s".format(response.status, response.body))
+      }
       case NOT_FOUND => Left(ItemNotFound)
       case _ => throw sys.error("Unexpected response: %d: %s".format(response.status, response.body))
     }
