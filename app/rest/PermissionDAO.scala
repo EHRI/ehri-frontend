8e7ee099483656367fb90e2f756d6d10ee743b03
Mike Bryant <mikesname@gmail.com>
2012-11-12 20:25:11 +0000

    Add url encoding in perms and visibility DAOs.

--- app/rest/PermissionDAO.scala
+++ app/rest/PermissionDAO.scala
@@ -26,20 +26,20 @@ case class PermissionDAO[T <: Accessor](val accessor: UserProfileRepr) extends R
   )
   
   def get: Future[Either[RestError, PermissionSet[UserProfileRepr]]] = {
-    WS.url(requestUrl).withHeaders(headers.toSeq: _*).get.map { response =>
+    WS.url(enc(requestUrl)).withHeaders(headers.toSeq: _*).get.map { response =>
         checkError(response).right.map(r => PermissionSet(accessor, r.json))
       }
   }
 
   def get(user: T): Future[Either[RestError, PermissionSet[T]]] = {
-    WS.url("%s/%s/%s".format(requestUrl, user.e.isA, user.identifier))
+    WS.url(enc("%s/%s/%s".format(requestUrl, user.e.isA, user.identifier)))
     	.withHeaders(headers.toSeq: _*).get.map { response =>
         checkError(response).right.map(r => PermissionSet[T](user, r.json))
       }
   }
     
   def set(user: T, data: Map[String, List[String]]): Future[Either[RestError, PermissionSet[T]]] = {
-    WS.url("%s/%s/%s".format(requestUrl, user.e.isA, user.identifier))
+    WS.url(enc("%s/%s/%s".format(requestUrl, user.e.isA, user.identifier)))
     	.withHeaders(headers.toSeq: _*).post(Json.generate(data)).map { response =>
         checkError(response).right.map(r => PermissionSet[T](user, r.json))
       }
