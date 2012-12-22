ecc313b66010662a45982a4a3b6b2a848930a224
Mike Bryant <mikesname@gmail.com>
2012-12-22 01:07:56 +0000

    Minor cleanup of still messy files

--- app/models/DocumentaryUnit.scala
+++ app/models/DocumentaryUnit.scala
@@ -14,17 +14,21 @@ import models.base.Field
 import models.base.Field._
 import models.base.TemporalEntity
 
-case class DocumentaryUnitRepr(val e: Entity) extends NamedEntity 
-		with AccessibleEntity
-		with HierarchicalEntity
-		with DescribedEntity
-		with TemporalEntity
-		with Formable[DocumentaryUnit] {
-  val holder: Option[AgentRepr] = e.relations(DocumentaryUnit.HELD_REL).headOption.map(AgentRepr(_))
-  val parent: Option[DocumentaryUnitRepr] = e.relations(DocumentaryUnit.CHILD_REL).headOption.map(DocumentaryUnitRepr(_))
-  val publicationStatus = e.property(DocumentaryUnit.PUB_STATUS).flatMap(enum(PublicationStatus).reads(_).asOpt)
-  override def descriptions: List[DocumentaryUnitDescriptionRepr] = e.relations(DescribedEntity.DESCRIBES_REL).map(DocumentaryUnitDescriptionRepr(_))
-  
+case class DocumentaryUnitRepr(val e: Entity) extends NamedEntity
+  with AccessibleEntity
+  with HierarchicalEntity
+  with DescribedEntity
+  with TemporalEntity
+  with Formable[DocumentaryUnit] {
+
+  import DocumentaryUnit._
+  import DescribedEntity._
+
+  val holder: Option[AgentRepr] = e.relations(HELD_REL).headOption.map(AgentRepr(_))
+  val parent: Option[DocumentaryUnitRepr] = e.relations(CHILD_REL).headOption.map(DocumentaryUnitRepr(_))
+  val publicationStatus = e.property(PUB_STATUS).flatMap(enum(PublicationStatus).reads(_).asOpt)
+  override def descriptions: List[DocumentaryUnitDescriptionRepr] = e.relations(DESCRIBES_REL).map(DocumentaryUnitDescriptionRepr(_))
+
   def to: DocumentaryUnit = new DocumentaryUnit(
     id = Some(e.id),
     identifier = identifier,
@@ -34,44 +38,47 @@ case class DocumentaryUnitRepr(val e: Entity) extends NamedEntity
   )
 }
 
-case class DocumentaryUnitDescriptionRepr(val e: Entity) extends Description with Formable[DocumentaryUnitDescription] {
+case class DocumentaryUnitDescriptionRepr(val e: Entity)
+  extends Description
+  with Formable[DocumentaryUnitDescription] {
+  import DocumentaryUnit._
   def to = new DocumentaryUnitDescription(
-	id = Some(e.id),
-	languageCode = languageCode,
-	title = e.property(DocumentaryUnit.TITLE).flatMap(_.asOpt[String]),
-	context = DocumentaryUnitContext(
-	  adminBiogHist = stringProperty(DocumentaryUnit.ADMIN_BIOG),
-	  archivalHistory = stringProperty(DocumentaryUnit.ARCH_HIST),
-	  acquisition = stringProperty(DocumentaryUnit.ACQUISITION)	  
-	),
-	content = DocumentaryUnitContent(
-		scopeAndContent = stringProperty(DocumentaryUnit.SCOPE_CONTENT),    
-		appraisal = stringProperty(DocumentaryUnit.APPRAISAL),
-		accruals = stringProperty(DocumentaryUnit.ACCRUALS),
-		systemOfArrangement = stringProperty(DocumentaryUnit.SYS_ARR)
-	),
-	conditions = DocumentaryUnitConditions(
-		conditionsOfAccess = stringProperty(DocumentaryUnit.ACCESS_COND),
-		conditionsOfReproduction = stringProperty(DocumentaryUnit.REPROD_COND),
-		physicalCharacteristics = stringProperty(DocumentaryUnit.PHYSICAL_CHARS),
-		findingAids = stringProperty(DocumentaryUnit.FINDING_AIDS)
-	),
-	materials = DocumentaryUnitMaterials(
-		locationOfOriginals = stringProperty(DocumentaryUnit.LOCATION_ORIGINALS),
-		locationOfCopies = stringProperty(DocumentaryUnit.LOCATION_COPIES),
-		relatedUnitsOfDescription = stringProperty(DocumentaryUnit.RELATED_UNITS),
-		publicationNote = stringProperty(DocumentaryUnit.PUBLICATION_NOTE)
-	)	
+    id = Some(e.id),
+    languageCode = languageCode,
+    title = e.property(TITLE).flatMap(_.asOpt[String]),
+    context = DocumentaryUnitContext(
+      adminBiogHist = stringProperty(ADMIN_BIOG),
+      archivalHistory = stringProperty(ARCH_HIST),
+      acquisition = stringProperty(ACQUISITION)
+    ),
+    content = DocumentaryUnitContent(
+      scopeAndContent = stringProperty(SCOPE_CONTENT),
+      appraisal = stringProperty(APPRAISAL),
+      accruals = stringProperty(ACCRUALS),
+      systemOfArrangement = stringProperty(SYS_ARR)
+    ),
+    conditions = DocumentaryUnitConditions(
+      conditionsOfAccess = stringProperty(ACCESS_COND),
+      conditionsOfReproduction = stringProperty(REPROD_COND),
+      physicalCharacteristics = stringProperty(PHYSICAL_CHARS),
+      findingAids = stringProperty(FINDING_AIDS)
+    ),
+    materials = DocumentaryUnitMaterials(
+      locationOfOriginals = stringProperty(LOCATION_ORIGINALS),
+      locationOfCopies = stringProperty(LOCATION_COPIES),
+      relatedUnitsOfDescription = stringProperty(RELATED_UNITS),
+      publicationNote = stringProperty(PUBLICATION_NOTE)
+    )
   )
 }
 
-object DocumentaryUnit  {
-  
+object DocumentaryUnit {
+
   final val DESC_REL = "describes"
   final val ACCESS_REL = "access"
   final val HELD_REL = "heldBy"
   final val CHILD_REL = "childOf"
-    
+
   /* ISAD(G)-based field set */
   val NAME = Field("name", "Name")
   val TITLE = Field("title", "Title")
@@ -98,13 +105,11 @@ case class DocumentaryUnit(
   val identifier: String,
   val name: String,
   val publicationStatus: Option[PublicationStatus.Value] = None,
-  
-  @Annotations.Relation(DocumentaryUnit.DESC_REL)
-  val descriptions: List[DocumentaryUnitDescription] = Nil
-) extends Persistable {
+
+  @Annotations.Relation(DocumentaryUnit.DESC_REL) val descriptions: List[DocumentaryUnitDescription] = Nil) extends Persistable {
   val isA = EntityType.DocumentaryUnit
 
-  def withDescription(d: DocumentaryUnitDescription): DocumentaryUnit = copy(descriptions=descriptions++List(d))
+  def withDescription(d: DocumentaryUnitDescription): DocumentaryUnit = copy(descriptions = descriptions ++ List(d))
 }
 
 case class DocumentaryUnitDescription(
@@ -114,35 +119,30 @@ case class DocumentaryUnitDescription(
   val context: DocumentaryUnitContext,
   val content: DocumentaryUnitContent,
   val conditions: DocumentaryUnitConditions,
-  val materials: DocumentaryUnitMaterials
-)  extends Persistable {
+  val materials: DocumentaryUnitMaterials) extends Persistable {
   val isA = EntityType.DocumentaryUnitDescription
-  
+
 }
 
 case class DocumentaryUnitContext(
-		val adminBiogHist: Option[String] = None,
-		val archivalHistory: Option[String] = None,
-		val acquisition: Option[String] = None
-) extends AttributeSet
+  val adminBiogHist: Option[String] = None,
+  val archivalHistory: Option[String] = None,
+  val acquisition: Option[String] = None) extends AttributeSet
 
 case class DocumentaryUnitContent(
-		val scopeAndContent: Option[String] = None,
-		val appraisal: Option[String] = None,
-		val accruals: Option[String] = None,
-		val systemOfArrangement: Option[String] = None
-) extends AttributeSet
+  val scopeAndContent: Option[String] = None,
+  val appraisal: Option[String] = None,
+  val accruals: Option[String] = None,
+  val systemOfArrangement: Option[String] = None) extends AttributeSet
 
 case class DocumentaryUnitConditions(
-		val conditionsOfAccess: Option[String] = None,
-		val conditionsOfReproduction: Option[String] = None,
-		val physicalCharacteristics: Option[String] = None,
-		val findingAids: Option[String] = None
-) extends AttributeSet
+  val conditionsOfAccess: Option[String] = None,
+  val conditionsOfReproduction: Option[String] = None,
+  val physicalCharacteristics: Option[String] = None,
+  val findingAids: Option[String] = None) extends AttributeSet
 
 case class DocumentaryUnitMaterials(
-		val locationOfOriginals: Option[String] = None,
-		val locationOfCopies: Option[String] = None,
-		val relatedUnitsOfDescription: Option[String] = None,
-		val publicationNote: Option[String] = None
-) extends AttributeSet
+  val locationOfOriginals: Option[String] = None,
+  val locationOfCopies: Option[String] = None,
+  val relatedUnitsOfDescription: Option[String] = None,
+  val publicationNote: Option[String] = None) extends AttributeSet
