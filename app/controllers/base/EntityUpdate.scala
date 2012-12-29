518496f19dc4c2dfaa8bf77c6b277775f09fce41
Mike Bryant <mikesname@gmail.com>
2012-12-29 17:44:47 +0000

    Fix fetching of item before reshowing errored form.

--- app/controllers/base/EntityUpdate.scala
+++ app/controllers/base/EntityUpdate.scala
@@ -2,7 +2,7 @@ package controllers.base
 
 import play.api.libs.concurrent.execution.defaultContext
 import models.base.AccessibleEntity
-import play.api.mvc.Call
+import play.api.mvc.{AsyncResult, Call}
 import models.base.Persistable
 import play.api.data.{ Form, FormError }
 import models.base.Formable
@@ -29,35 +29,40 @@ trait EntityUpdate[F <: Persistable, T <: AccessibleEntity with Formable[F]] ext
   def updatePost(id: String) = withItemPermission(id, PermissionType.Update) { implicit user =>
     implicit request =>
       implicit val maybeUser = Some(user)
-      form.bindFromRequest.fold(
-        errorForm => {
-          AsyncRest {
-            rest.EntityDAO(entityType, maybeUser).get(id).map { itemOrErr =>
-              itemOrErr.right.map { item =>
-                val doc: T = builder(item)
-                BadRequest(formView(Some(doc), errorForm, updateAction(id), user, request))
-              }
-            }
+
+      def renderForm(form: Form[F]): AsyncResult = AsyncRest {
+        rest.EntityDAO(entityType, maybeUser).get(id).map { itemOrErr =>
+          itemOrErr.right.map { item =>
+            val doc: T = builder(item)
+            BadRequest(formView(Some(doc), form, updateAction(id), user, request))
           }
-        },
-        doc => {
+        }
+      }
+
+      form.bindFromRequest.fold(
+        errorForm => renderForm(errorForm),
+        success = doc => {
           AsyncRest {
             rest.EntityDAO(entityType, maybeUser)
-              .update(id, doc.toData).map { itemOrErr =>
-                // If we have an error, check if it's a validation error.
-                // If so, we need to merge those errors back into the form
-                // and redisplay it...
-                if (itemOrErr.isLeft) {
-                  itemOrErr.left.get match {
+              .update(id, doc.toData).map {
+              itemOrErr =>
+              // If we have an error, check if it's a validation error.
+              // If so, we need to merge those errors back into the form
+              // and redisplay it...
+
+                itemOrErr.fold(
+                  err => err match {
                     case err: rest.ValidationError => {
+
                       val serverErrors: Seq[FormError] = doc.errorsToForm(err.errorSet)
                       val filledForm = form.fill(doc).copy(errors = form.errors ++ serverErrors)
-                      Right(BadRequest(formView(None, filledForm, updateAction(id), user, request)))
+                      Right(renderForm(filledForm))
                     }
                     case e => Left(e)
-                  }
-                } else itemOrErr.right.map { item => Redirect(showAction(item.id)) }
-              }
+                  },
+                  item => Right(Redirect(showAction(item.id)))
+                )
+            }
           }
         }
       )
