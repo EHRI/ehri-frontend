a830545ef30bf66d596c8821920d716ade408dfb
Mike Bryant <mikesname@gmail.com>
2012-12-30 00:36:31 +0000

    Comment and formatting

--- app/controllers/base/EntityRead.scala
+++ app/controllers/base/EntityRead.scala
@@ -6,6 +6,12 @@ import play.api.mvc.RequestHeader
 import play.api.mvc.Call
 import models.UserProfile
 
+/**
+ * Controller trait which handles the listing and showing of Entities that
+ * implement the AccessibleEntity trait.
+ *
+ * @tparam T
+ */
 trait EntityRead[T <: AccessibleEntity] extends EntityController[T] {
   val DEFAULT_LIMIT = 20
 
@@ -16,13 +22,14 @@ trait EntityRead[T <: AccessibleEntity] extends EntityController[T] {
   val showAction: String => Call
   val showView: ShowViewType
 
-  import com.codahale.jerkson.Json
-
   def getJson(id: String) = userProfileAction { implicit maybeUser =>
     implicit request =>
+      import com.codahale.jerkson.Json
       AsyncRest {
         rest.EntityDAO(entityType, maybeUser).get(id).map { itemOrErr =>
-          itemOrErr.right.map { item => Ok(Json.generate(item.data)) }
+          itemOrErr.right.map {
+            item => Ok(Json.generate(item.data))
+          }
         }
       }
   }
@@ -31,7 +38,9 @@ trait EntityRead[T <: AccessibleEntity] extends EntityController[T] {
     implicit request =>
       AsyncRest {
         rest.EntityDAO(entityType, maybeUser).get(id).map { itemOrErr =>
-          itemOrErr.right.map { item => Ok(showView(builder(item), maybeUser, request)) }
+          itemOrErr.right.map {
+            item => Ok(showView(builder(item), maybeUser, request))
+          }
         }
       }
   }
@@ -41,8 +50,10 @@ trait EntityRead[T <: AccessibleEntity] extends EntityController[T] {
       AsyncRest {
         rest.EntityDAO(entityType, maybeUser)
           .page(math.max(page, 1), math.max(limit, 1)).map { itemOrErr =>
-            itemOrErr.right.map { lst => Ok(listView(lst.copy(list = lst.list.map(builder(_))), showAction, maybeUser, request)) }
+          itemOrErr.right.map {
+            lst => Ok(listView(lst.copy(list = lst.list.map(builder(_))), showAction, maybeUser, request))
           }
+        }
       }
   }
 }
