fd778f51984b4edd16f0b2dfe7c6ff7fdcaeb6a2
Mike Bryant <mikesname@gmail.com>
2013-02-14 16:54:24 +0000

    Add an (untested) method to determine if a section is empty

--- app/models/base/AttributeSet.scala
+++ app/models/base/AttributeSet.scala
@@ -10,6 +10,24 @@ trait AttributeSet {
     f.setAccessible(true)
     a + (f.getName -> toValue(f.get(this)))
   }
+
+  /**
+   * Use reflection to determine if this Attribute set contains
+   * only None or empty string values - if so it's considered
+   * 'empty'. If there are *any* non-Option[String] or non
+   * string values this returns false
+   */
+  def isEmpty: Boolean = {
+    getClass.getDeclaredFields.foreach { f =>
+      f.setAccessible(true)
+      f.get(this) match {
+        case opt: Option[_] => if (opt.isDefined) return false
+        case s: String => if (!s.isEmpty) return false
+        case v => return false
+      }
+    }
+    true
+  }
   
   def toValue(v: Any): Any = v match {
       case Some(value) => value
