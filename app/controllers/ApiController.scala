2b8fb436cbd683d343bfa90b2b77f0db403c5243
Mike Bryant <mikesname@gmail.com>
2013-02-15 17:14:43 +0000

    Add convenience methods for routing

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -1,14 +1,25 @@
 package controllers
 
 import controllers.base.AuthController
-import play.api.mvc.Controller
+import play.api.mvc.{Action, Controller, SimpleResult, ResponseHeader}
 import controllers.base.ControllerHelpers
-import play.api.mvc.SimpleResult
-import play.api.mvc.ResponseHeader
 import play.api.libs.iteratee.Enumerator
 import play.api.libs.concurrent.Execution.Implicits._
 
+/**
+ * Simple proxy controller that authorizes users and passes a request
+ * directly on the the REST server. Currently only GET actions are
+ * supported.
+ */
 object ApiController extends Controller with AuthController with ControllerHelpers {
+
+  def listItems(contentType: String) = Action { implicit request =>
+    get(s"$contentType/list")(request)
+  }
+
+  def getItem(contentType: String, id: String) = Action { implicit request =>
+    get(s"$contentType/$id")(request)
+  }
 
   def get(urlpart: String) = userProfileAction { implicit maybeUser =>
     implicit request =>
