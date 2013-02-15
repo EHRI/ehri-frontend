4e959d2a4852c17715832d07c9366f0e127be0c7
Mike Bryant <mikesname@gmail.com>
2013-02-15 17:14:19 +0000

    As-yet-unsuccessful JSON tests

--- app/models/json/DocumentaryUnitFormat.scala
+++ app/models/json/DocumentaryUnitFormat.scala
@@ -6,37 +6,82 @@ import play.api.libs.json.util._
 
 
 import defines.PublicationStatus
-import models.DocumentaryUnitDescriptionF
+import models.{Annotations, DatePeriodF, DocumentaryUnitDescriptionF}
 import models.DocumentaryUnitDescriptionF._
-import models.base.DescribedEntity
+import models.base.{TemporalEntity, DescribedEntity}
+import org.joda.time.DateTime
+
+case class TestDatePeriod(
+  id: Option[String],
+  startDate: Option[DateTime],
+  endDate: Option[DateTime] = None
+)
+
+case class TestData(id: String, foo: String, bar: String)
 
 case class TestDesc(
   id: Option[String],
   identifier: String,
+  title: Option[String],
+  languageCode: String,
   context: Context,
   content: Content,
   conditions: Conditions,
   materials: Materials,
-  control: Control
+  control: Control,
+  @Annotations.Relation(TemporalEntity.DATE_REL)
+  dates: List[TestDatePeriod]
 )
 
 case class TestDocumentaryUnit(
   id: Option[String],
   identifier: String,
   name: String,
-  publicationStatus: Option[PublicationStatus.Value] = None,
-  descriptions: List[TestDesc] = Nil
+  publicationStatus: Option[PublicationStatus.Value],
+  descriptions: List[TestDesc]
 )
 
 object DocumentaryUnitFormat {
   import models.Entity._
   import models.IsadG._
 
+  implicit val testCaseFormat: Format[TestData] = (
+    (__ \ "id").format[String] and
+    (__ \ "data" \ "foo").format[String] and
+    (__ \ "data" \ "bar").format[String]
+  )(TestData.apply, unlift(TestData.unapply))
+
+  implicit val datePeriodReads: Reads[TestDatePeriod] = (
+    (__ \ ID).readNullable[String] and
+      (__ \ DATA \ DatePeriodF.START_DATE).readNullable[DateTime] and
+      (__ \ DATA \ DatePeriodF.END_DATE).readNullable[DateTime]
+    )(TestDatePeriod)
+
+  implicit val datePeriodWrites: Writes[TestDatePeriod] = (
+    (__ \ ID).writeNullable[String] and
+      (__ \ DATA \ DatePeriodF.START_DATE).write[Option[DateTime]] and
+      (__ \ DATA \ DatePeriodF.END_DATE).write[Option[DateTime]]
+    )(unlift(TestDatePeriod.unapply))
+
+  implicit val datePeriodFormat: Format[TestDatePeriod] = Format(datePeriodReads,datePeriodWrites)
+
+  /*implicit val datePeriodListFormat: Format[TestDatePeriod] = (
+      (__ \ ID).formatNullable[String] and
+      (__ \ DATA \ DatePeriodF.START_DATE).formatNullable[DateTime] and
+      (__ \ DATA \ DatePeriodF.END_DATE).formatNullable[DateTime]
+    )(TestDatePeriod.apply, unlift(TestDatePeriod.unapply))*/
+
   implicit val pubStatusFormat = defines.EnumFormat.enumFormat(PublicationStatus)
   
+
+  // NON-Working Format -items in the data section get overwritten
+  // by subsequent items.
+
   implicit val descFormat: Format[TestDesc] = (
     (__ \ ID).formatNullable[String] and
     (__ \ DATA \ IDENTIFIER).format[String] and
+    (__ \ DATA \ TITLE).formatNullable[String] and
+    (__ \ DATA \ LANG_CODE).format[String] and
     (__ \ DATA).format[Context]((
       (__ \ ADMIN_BIOG).formatNullable[String] and
       (__ \ ARCH_HIST).formatNullable[String] and
@@ -64,25 +109,20 @@ object DocumentaryUnitFormat {
         (__ \ ARCHIVIST_NOTE).formatNullable[String] and
         (__ \ RULES_CONVENTIONS).formatNullable[String] and
         (__ \ DATES_DESCRIPTIONS).formatNullable[String]
-      )(Control.apply, unlift(Control.unapply)))
+      )(Control.apply, unlift(Control.unapply))) and
+      (__ \ RELATIONSHIPS \ TemporalEntity.DATE_REL).lazyFormat[List[TestDatePeriod]](
+      Reads.list[TestDatePeriod], Writes.traversableWrites[TestDatePeriod])
     )(TestDesc.apply, unlift(TestDesc.unapply))
 
-  implicit val documentaryUnitReads: Reads[TestDocumentaryUnit] = (
-    (__ \ ID).readNullable[String] and
-    (__ \ DATA \ IDENTIFIER).read[String] and
-    (__ \ DATA \ NAME).read[String] and
-    (__ \ DATA \ PUB_STATUS).readNullable[PublicationStatus.Value] and
-    (__ \ RELATIONSHIPS \ DescribedEntity.DESCRIBES_REL).lazyRead(Reads.list[TestDesc])
-  )(TestDocumentaryUnit)
 
-  implicit val documentaryUnitWrites: Writes[TestDocumentaryUnit] = (
-    (__ \ ID).writeNullable[String] and
-      (__ \ DATA \ IDENTIFIER).write[String] and
-      (__ \ DATA \ NAME).write[String] and
-      (__ \ DATA \ PUB_STATUS).writeNullable[PublicationStatus.Value] and
-      (__ \ RELATIONSHIPS \ DescribedEntity.DESCRIBES_REL).lazyWrite(Writes.traversableWrites[TestDesc])
-    )(unlift(TestDocumentaryUnit.unapply))
 
-  implicit val documentaryUnitFormat: Format[TestDocumentaryUnit] = Format(documentaryUnitReads,documentaryUnitWrites)
-  
+
+  implicit val documentaryUnitFormat: Format[TestDocumentaryUnit] = (
+    (__ \ ID).formatNullable[String] and
+      (__ \ DATA \ IDENTIFIER).format[String] and
+      (__ \ DATA \ NAME).format[String] and
+      (__ \ DATA \ PUB_STATUS).formatNullable[PublicationStatus.Value] and
+      (__ \ RELATIONSHIPS \ DescribedEntity.DESCRIBES_REL).lazyFormat[List[TestDesc]](
+        Reads.list[TestDesc], Writes.traversableWrites[TestDesc])
+    )(TestDocumentaryUnit.apply, unlift(TestDocumentaryUnit.unapply))
 }
---
+++ test/json/JsonFormatSpec.scala
@@ -0,0 +1,47 @@
+package test.json
+
+import org.specs2.mutable._
+
+import play.api.test._
+import play.api.test.Helpers._
+import play.api.i18n.Messages
+import play.api.libs.json.Json
+import models.json.TestDocumentaryUnit
+
+class JsonFormatSpec extends Specification {
+
+  "Documentary Unit Format should read and write with no changes" in {
+    import models.json._
+    import models.json.DocumentaryUnitFormat._
+
+    //val validation = Json.parse(json.documentaryUnit).validate[TestDocumentaryUnit]
+
+    val data =
+      """
+        {
+          "id": "id-1",
+          "data": {
+            "foo": "teststr1",
+            "bar": "teststr2"
+          }
+        }
+      """
+
+    val validation = Json.parse(data).validate[TestData]
+    validation.asEither must beRight
+    println(validation.get)
+    println(Json.toJson(validation.get))
+
+    //validation.asEither must beRight
+    //val doc = validation.get
+    /*println("DOC: " + doc)
+    doc.descriptions.map { d =>
+      println("DESC: " + d)
+      println("DESC JSON: " + Json.toJson(d))
+    }
+    println("JSON: " + Json.toJson(doc))
+
+    Json.toJson(doc).as[TestDocumentaryUnit] must beEqualTo(doc)*/
+  }
+
+}
