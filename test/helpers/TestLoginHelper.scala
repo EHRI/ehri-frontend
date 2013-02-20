cec97af6d3f8d19eaa10881047b1b69b4942e87e
Mike Bryant <mikesname@gmail.com>
2013-02-20 16:08:39 +0000

    Add a fake CSRF token to the session. When forms are rendered it's expected to be there, and since we turn off CSRF for tests it won't be unless we add it here.

--- test/helpers/TestLoginHelper.scala
+++ test/helpers/TestLoginHelper.scala
@@ -17,17 +17,24 @@ trait TestLoginHelper {
 
   val LOGIN_PATH = routes.Application.login.url
 
-  def fakeLoginApplication(userProfile: String, additionalConfiguration: Map[String, Any] = Map()) = {
+  def fakeLoginApplication(userProfile: String, additionalConfiguration: Map[String, Any] = Map(), global: GlobalSettings = FakeGlobal) = {
     FakeApplication(
       additionalConfiguration = additionalConfiguration ++ Map("test.user.profile_id" -> userProfile),
-      additionalPlugins = Seq("mocks.MockUserDAO", "mocks.MockLoginHandler")
+      additionalPlugins = Seq("mocks.MockUserDAO", "mocks.MockLoginHandler"),
+      withGlobal = Some(global)
     )
   }
 
   def fakeLoggedInRequest(rtype: String, path: String) = {
+    // Login and add the auth cookie to the session.
     val cookies = header(HeaderNames.SET_COOKIE,
       route(play.api.test.FakeRequest(POST, LOGIN_PATH)).get)
       .getOrElse(sys.error("No Authorization cookie found"))
-    FakeRequest(rtype, path).withHeaders(HeaderNames.COOKIE -> cookies)
+    val fr = FakeRequest(rtype, path).withHeaders(HeaderNames.COOKIE -> cookies)
+
+    // Since we use csrf in forms, even though it's disabled in
+    // tests we still need to add a fake token to the session so
+    // the token is there when the form tries to render it.
+    fr.withSession(CSRF.Conf.TOKEN_NAME -> "fake-csrf-token")
   }
 }
