edef23fb70ce56a0300b5df9b5071bbeb7d6c128
Mike Bryant <mikesname@gmail.com>
2013-02-21 11:48:36 +0000

    Rough implementation of an inline date system that works. It doesn't currently do any housekeeping to maintain indices when items are deleted. Play actually doesn't seem to require this in order to work, which is interesting.

--- public/javascripts/docview.js
+++ public/javascripts/docview.js
@@ -4,10 +4,17 @@ jQuery(function($) {
   $(".chzn-select").chosen();
 
   // Delete inline date period tables
-  var wrapper = $("#date-row-template");
-  $(".remove-inline-date").click(function(event) {
-    var elem = $(this).closest(".inline-date");
-      $(".inline-date").length > 1 ? elem.remove() : elem.detach().appendTo(wrapper);
+  $(".inline-date-set").on("click", ".remove-inline-date", function(event) {
+    $(this).closest(".inline-date").remove();
+    event.preventDefault();
+  });
+
+  $(".inline-date-set").on("click", ".add-inline-date", function(event) {
+    var container = $(event.target).closest(".inline-date-set");
+    var template = $(".inline-date-template", container);
+    var idx = $(".inline-date", container).length;
+    var elem = $(template.html().replace(/IDX/g, idx));
+    container.append(elem);
     event.preventDefault();
   });
 });
