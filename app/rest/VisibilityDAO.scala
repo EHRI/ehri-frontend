86e02e758172e6470db529a87b47e73557b7c0c7
Mike Bryant <mikesname@gmail.com>
2013-03-04 15:37:29 +0000

    Return an item on visibility update, instead of nowt

--- app/rest/VisibilityDAO.scala
+++ app/rest/VisibilityDAO.scala
@@ -6,16 +6,17 @@ import play.api.libs.ws.WS
 import play.api.libs.concurrent.Execution.Implicits._
 import scala.concurrent.Future
 import play.api.libs.ws.WS
-import models.UserProfile
+import models.{Entity, UserProfile}
+
 
 case class VisibilityDAO(userProfile: Option[UserProfile]) extends RestDAO {
 
   def requestUrl = "http://%s:%d/%s/access".format(host, port, mount)
 
-  def set(id: String, data: List[String]): Future[Either[RestError, Boolean]] = {
+  def set(id: String, data: List[String]): Future[Either[RestError, Entity]] = {
     val params = "?" + data.map(p => "%s=%s".format(RestPageParams.ACCESSOR_PARAM, p)).mkString("&")
     WS.url(enc(requestUrl, id, params)).withHeaders(authHeaders.toSeq: _*).post("").map { response =>
-        checkError(response).right.map(r => true)
+        checkError(response).right.map(r => EntityDAO.handleUpdate(EntityDAO.jsonToEntity(r.json)))
       }
   }
 }
