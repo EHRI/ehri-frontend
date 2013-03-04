737e6178ba64f889c0886e7debb69f239d9d67fc
Mike Bryant <mikesname@gmail.com>
2013-03-04 17:05:58 +0000

    Add a new controller trait to add search functionality

---
+++ app/controllers/base/EntitySearch.scala
@@ -0,0 +1,51 @@
+package controllers.base
+
+import play.api.mvc._
+import play.api.libs.concurrent.Execution.Implicits._
+import models.base.AccessibleEntity
+import defines.PermissionType
+import models.{Entity, UserProfile}
+import solr.facet.{AppliedFacet, FacetClass, FacetData}
+import solr.SearchParams
+
+/**
+ * Controller trait for deleting AccessibleEntities.
+ *
+ * @tparam T the Entity's built representation
+ */
+trait EntitySearch[T <: AccessibleEntity] extends EntityRead[T] {
+
+  /**
+   * Inheriting controllers should override a list of facets that
+   * are available for the given entity type.
+   */
+  val entityFacets: List[FacetClass]
+
+  /**
+   * Action that restricts the search to the inherited entity type
+   * and applies
+   * @param f
+   * @return
+   */
+  def searchAction(f: solr.ItemPage[Entity] => SearchParams => List[AppliedFacet] => Option[UserProfile] => Request[AnyContent] => Result) = {
+    userProfileAction { implicit userOpt => implicit request =>
+
+      import solr._
+      val sp = SearchParams.form.bindFromRequest.value.get.copy(entity = Some(entityType))
+      val facets: List[AppliedFacet] = FacetData.bindFromRequest(entityFacets)
+      AsyncRest {
+        SolrDispatcher(userOpt).list(sp, facets).map { resOrErr =>
+          resOrErr.right.map { res =>
+            AsyncRest {
+              rest.SearchDAO(userOpt).list(res.items.map(_.id)).map { listOrErr =>
+                listOrErr.right.map { list =>
+                  f(res.copy(items = list))(sp)(facets)(userOpt)(request)
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
