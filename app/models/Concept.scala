3a5eb79de1f4d0aae2ff005b9317b257eb6b862d
Mike Bryant <mikesname@gmail.com>
2013-03-04 11:49:49 +0000

    Tweak display of parent items in search list

--- app/models/Concept.scala
+++ app/models/Concept.scala
@@ -9,6 +9,7 @@ import models.base.DescribedEntity
 import defines.EntityType
 import play.api.libs.json.Json
 import defines.EnumWriter.enumWrites
+import play.api.i18n.Lang
 
 object ConceptType extends Enumeration {
   type Type = Value
@@ -129,6 +130,11 @@ case class Concept(e: Entity)
 
   // Because we (currently) have no 'name' property on Concept, get the first available preflabel
   override def toString = descriptions.headOption.flatMap(_.stringProperty(ConceptF.PREFLABEL)).getOrElse(identifier)
+
+  // Language-aware toString
+  def toString(implicit lang: Lang) = descriptions
+    .find(_.formable.languageCode==lang.code).orElse(descriptions.headOption)
+    .flatMap(_.stringProperty(ConceptF.PREFLABEL)).getOrElse(identifier)
 }
 
 case class ConceptDescription(val e: Entity)
