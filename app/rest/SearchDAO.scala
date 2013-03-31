3d28359f26381007e8bb5b32510a733ff515c416
Mike Bryant <mikesname@gmail.com>
2013-03-31 14:36:49 +0100

    Allow the search list fetch to use POST for really long lists of ids

--- app/rest/SearchDAO.scala
+++ app/rest/SearchDAO.scala
@@ -4,15 +4,15 @@ import play.api.libs.concurrent.Execution.Implicits._
 import scala.concurrent.Future
 import play.api.libs.ws.WS
 import models.{UserProfile, Entity}
-
+import play.api.libs.json.Json
 
 case class SearchDAO(userProfile: Option[UserProfile]) extends RestDAO {
 
   def requestUrl = "http://%s:%d/%s/entities".format(host, port, mount)
 
   def list(ids: Seq[String]): Future[Either[RestError, List[Entity]]] = {
-    WS.url(enc(requestUrl, "?" + (ids.map("id=" + _).mkString("&")))).withHeaders(authHeaders.toSeq: _*)
-      .get.map { response =>
+    WS.url(requestUrl).withHeaders(authHeaders.toSeq: _*)
+      .post(Json.toJson(ids)).map { response =>
         checkError(response).right.map { r =>
           r.json.validate[List[models.Entity]].fold(
             valid = { list => list },
