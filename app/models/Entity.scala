b66f842eb813aef325ded5928cfb8269410fa0a5
Mike Bryant <mikesname@gmail.com>
2013-04-14 15:08:47 +0100

    Allow the listProperty helper to take a string as a fallback.

--- app/models/Entity.scala
+++ app/models/Entity.scala
@@ -6,14 +6,14 @@ import play.api.libs.json._
 
 
 object Entity {
-  
+
   val ID = "id"
   val IDENTIFIER = "identifier"
   val ISA = "isA"
   val DATA = "data"
   val TYPE = "type"
-  val RELATIONSHIPS = "relationships"  
-  
+  val RELATIONSHIPS = "relationships"
+
   def fromString(s: String, t: EntityType.Value) = {
     new Entity(s, t, Map(IDENTIFIER -> JsString(s)), Map())
   }
@@ -49,13 +49,13 @@ case class Entity(
   relationships: Map[String, List[Entity]] = Map()) {
 
   def property(name: String) = data.get(name)
-  
+
   /**
    * Shortcut for fetching a Option[String] property.
    */
   def stringProperty(name: String): Option[String] = property(name).flatMap(_.asOpt[String])
-  def listProperty(name: String): Option[List[String]] = property(name).flatMap(_.asOpt[List[String]])
-  def relations(s: String): List[Entity] = relationships.getOrElse(s, List())  
+  def listProperty(name: String): Option[List[String]] = property(name).flatMap(v => v.asOpt[List[String]] orElse v.asOpt[String].map(List(_)) )
+  def relations(s: String): List[Entity] = relationships.getOrElse(s, List())
   def withProperty(name: String, value: JsValue) = copy(data=data + (name -> value))
   def withRelation(s: String, r: Entity) = {
     val list: List[Entity] = relationships.getOrElse(s, Nil)
