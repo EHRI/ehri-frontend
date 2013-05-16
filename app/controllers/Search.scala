8f6b8c07dd489dea01a5ee8a093ffaaf2605a9c1
Mike Bryant <mikesname@gmail.com>
2013-05-16 17:26:57 +0100

    Attempt to simplify the rebuild of the search index by utilizing the new count() function and listing (with offset) rather than paging the content. This should be easier on the server as well since it doesn't always have to iterate every item for paging, for every page. Still inadequately tested, and with inadequate error handling.

--- app/controllers/Search.scala
+++ app/controllers/Search.scala
@@ -121,17 +121,17 @@ object Search extends EntitySearch {
     /**
      * Update a single page of data
      */
-    def updatePage(entityType: EntityType.Value, page: rest.Page[Entity], chan: Concurrent.Channel[String]
+    def updatePage(entityType: EntityType.Value, page: Int, list: List[Entity], chan: Concurrent.Channel[String]
                     ): Future[List[SolrResponse]] = {
-      solr.SolrIndexer.updateItems(page.items.toStream, commit = false).map { jobs =>
+      solr.SolrIndexer.updateItems(list.toStream, commit = false).map { jobs =>
         jobs.map { response =>
           response match {
             case e: SolrErrorResponse => {
-              Logger.logger.error(s"Unable to page page data for entity: $entityType, page: ${page.page}: {}", e.err)
+              Logger.logger.error(s"Unable to page page data for entity: $entityType, page: ${list}: {}", e.err)
               e
             }
             case ok: SolrUpdateResponse => {
-              val msg = s"Batch complete: $entityType (${page.range}}, time: ${ok.time})"
+              val msg = s"Batch complete: $entityType (${page}}, time: ${ok.time})"
               Logger.logger.info(msg)
               chan.push(wrapMsg(msg))
               ok
@@ -147,14 +147,14 @@ object Search extends EntitySearch {
     def updateItemSet(entityType: EntityType.Value, pageNum: Int,
                       chan: Concurrent.Channel[String]): Future[List[SolrResponse]] = {
       val getData = EntityDAO(entityType, userOpt)
-            .page(RestPageParams(page=Some(pageNum), limit = Some(batchSize)))
-      getData.flatMap { pageOrErr =>
-        pageOrErr match {
+            .list(RestPageParams(page=Some(pageNum), limit = Some(batchSize)))
+      getData.flatMap { listOrErr =>
+        listOrErr match {
           case Left(err) => {
             Logger.logger.error(s"Unable to page page data for entity: $entityType, page: ${pageNum}: {}", err)
-            Future.successful(List(SolrErrorResponse(pageOrErr.left.get)))
+            Future.successful(List(SolrErrorResponse(listOrErr.left.get)))
           }
-          case Right(page) => updatePage(entityType, pageOrErr.right.get, chan)
+          case Right(page) => updatePage(entityType, pageNum, listOrErr.right.get, chan)
         }
       }
     }
@@ -162,11 +162,11 @@ object Search extends EntitySearch {
     // Create an unicast channel in which to feed progress messages
     val channel = Concurrent.unicast[String] { chan =>
       var all: List[Future[List[SolrResponse]]] = entities.map { entity =>
-        EntityDAO(entity, userOpt).page(RestPageParams(limit = Some(batchSize))).flatMap { firstPageOrErr =>
-          if (firstPageOrErr.isLeft) {
-            sys.error(s"Unable to fetch first page of data for $entity: " + firstPageOrErr.left.get)
+        EntityDAO(entity, userOpt).count().flatMap { countOrErr =>
+          if (countOrErr.isLeft) {
+            sys.error(s"Unable to fetch first page of data for $entity: " + countOrErr.left.get)
           }
-          val firstPage = firstPageOrErr.right.get
+          val count = countOrErr.right.get
 
           // Clear all Entities from the index...
           var allUpdateResponses: Future[List[SolrResponse]] = solr.SolrIndexer.deleteItemsByType(entity, commit = false).flatMap { response =>
@@ -176,16 +176,13 @@ object Search extends EntitySearch {
                 Future.successful(List(response))
               }
               case ok => {
-                // Since we've already fetched a page of data, reuse it immediately
-                var page1: Future[List[SolrResponse]] = updatePage(entity, firstPage, chan)
-
-                // Run the rest in sequence
-                var rest: List[Future[List[SolrResponse]]] = 2.to(firstPage.numPages.toInt).map { p =>
+                // Run each page in sequence...
+                var pages: List[Future[List[SolrResponse]]] = 1L.to(count).map { p =>
                     updateItemSet(entity, p.toInt, chan)
                 }.toList
 
                 // Flatten the inner batch results into a single list
-                Future.sequence(page1 :: rest).map(l => l.flatMap(i => i))
+                Future.sequence(pages).map(l => l.flatMap(i => i))
               }
             }
           }
