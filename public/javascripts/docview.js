66be43e31231f791148cdfcfe1adfbc0d8eb1d78
Mike Bryant <mikesname@gmail.com>
2013-05-16 16:39:16 +0100

    Miscellaneous JS fixes/enhancements

--- public/javascripts/docview.js
+++ public/javascripts/docview.js
@@ -1,15 +1,65 @@
 jQuery(function($) {
 
+  /**
+   * jQuery plugin that makes an element 'stick' to the bottom
+   * of the viewport if it is outside. Used for form action
+   * sections containing the submit button.
+   */
+  $.fn.stickyFormFooter = function() {
+    if(this.length > 0) {
+      var that = this;
+      var top = that.offset().top;
+      var height = that.outerHeight();
+      var innerHeight = that.height();
+
+      function shouldStick() {
+        var vpend = $(window).outerHeight() + $(window).scrollTop();
+        var sticky = top > vpend + innerHeight;
+
+        if (sticky) {
+          if (!that.isSticky === sticky) {
+            that.css({
+              position: "fixed",
+              left: 0,
+              width: $(window).width(),
+              top: $(window).outerHeight() - height
+            }).addClass("sticky");
+            that.isSticky = sticky;
+          }
+        } else {
+          if (!that.isSticky === sticky) {
+            that.removeAttr("style")
+                .removeClass("sticky");
+            that.isSticky = null;
+          }
+        }
+      }
+
+      $(window).scroll(shouldStick);
+      shouldStick();
+    }
+  }
+
+  $(".form-actions").stickyFormFooter();
+
   // Add Bootstrap tooltip on input boxes with a title.
   // Filter items with an empty title.
-  $("input[title],textarea[title]").filter(function(e) {
-    return $.trim($(this).prop("title")) != "";
-  }).popover({
-    html: true,
-    delay:1000,
-    trigger: "blur",
-    placement: "right"
+  $("input[title!=''],textarea[title!='']").each(function() {
+      var that = $(this);
+      that.attr("data-content", that.attr("title"));
+      that.attr("title", that.parents(".control-group").find(".control-label").text());
+      that.popover({
+        html: true,
+        delay:{
+          show: 500,
+          hide: 100
+        },
+        trigger: "blur",
+        placement: "right"
+      });
   });
+
+
 
   // Click to expand additonal data sections
   $("table.unknown-data").hide();
