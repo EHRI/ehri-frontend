2e7574864eb9f55c21eb5eee6c1ab4015dfb0745
Mike Bryant <mikesname@gmail.com>
2013-06-10 15:24:36 +0100

    Add a crude way to experiment with SPARQL

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -5,6 +5,7 @@ import play.api.mvc.{Action, Controller}
 import controllers.base.ControllerHelpers
 import play.api.libs.iteratee.Enumerator
 import play.api.libs.concurrent.Execution.Implicits._
+import java.net.URLEncoder
 
 /**
  * Simple proxy controller that authorizes users and passes a request
@@ -24,6 +25,25 @@ object ApiController extends Controller with AuthController with ControllerHelpe
   def getAny(id: String) = Action { implicit request =>
     get(s"entities?id=$id")(request)
   }
+
+  import play.api.data.Form
+  import play.api.data.Forms._
+  private val sparqlForm = Form(single("q" -> text))
+
+  def sparql = userProfileAction { implicit userOpt => implicit request =>
+    Ok(views.html.sparqlForm(sparqlForm, routes.ApiController.sparqlQuery))
+  }
+
+  def sparqlQuery = userProfileAction { implicit userOpt => implicit request =>
+    Async {
+      rest.ApiDAO(userOpt).get("sparql", request.queryString, request.headers).map { r =>
+        Status(r.status)
+          .stream(Enumerator.fromStream(r.ahcResponse.getResponseBodyAsStream))
+          .as(r.ahcResponse.getContentType)
+      }
+    }
+  }
+
 
   def get(urlpart: String) = userProfileAction { implicit maybeUser =>
     implicit request =>
