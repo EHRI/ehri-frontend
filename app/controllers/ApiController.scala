80302378f3461e01343ac9a415eefd26c8ff4e84
Mike Bryant <mikesname@gmail.com>
2013-06-10 22:00:48 +0100

    Add some default sparql code for testing

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -26,25 +26,6 @@ object ApiController extends Controller with AuthController with ControllerHelpe
     get(s"entities?id=$id")(request)
   }
 
-  import play.api.data.Form
-  import play.api.data.Forms._
-  private val sparqlForm = Form(single("q" -> text))
-
-  def sparql = userProfileAction { implicit userOpt => implicit request =>
-    Ok(views.html.sparqlForm(sparqlForm, routes.ApiController.sparqlQuery))
-  }
-
-  def sparqlQuery = userProfileAction { implicit userOpt => implicit request =>
-    Async {
-      rest.ApiDAO(userOpt).get("sparql", request.queryString, request.headers).map { r =>
-        Status(r.status)
-          .stream(Enumerator.fromStream(r.ahcResponse.getResponseBodyAsStream))
-          .as(r.ahcResponse.getContentType)
-      }
-    }
-  }
-
-
   def get(urlpart: String) = userProfileAction { implicit maybeUser =>
     implicit request =>
       Async {
@@ -56,5 +37,46 @@ object ApiController extends Controller with AuthController with ControllerHelpe
               .as(r.ahcResponse.getContentType)
           }
       }
+  }
+
+  //
+  // Test guff for the Sparql endpoint...
+  //
+
+  import play.api.data.Form
+  import play.api.data.Forms._
+  private val sparqlForm = Form(single("q" -> text))
+
+  private val defaultSparql =
+    """
+      |PREFIX edge:   <http://tinkerpop.com/pgm/edge/>
+      |PREFIX vertex: <http://tinkerpop.com/pgm/vertex/>
+      |PREFIX prop:   <http://tinkerpop.com/pgm/property/>
+      |PREFIX pgm:    <http://tinkerpop.com/pgm/ontology#>
+      |PREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      |
+      |# Select all the userProfile nodes and their name properties...
+      |SELECT ?n ?u WHERE {
+      |    ?u a pgm:Vertex ;
+      |       prop:__ISA__  "userProfile" ;
+      |       prop:name     ?n .
+      |}
+      |
+      |LIMIT 100
+    """.stripMargin
+
+  def sparql = userProfileAction { implicit userOpt => implicit request =>
+    Ok(views.html.sparqlForm(sparqlForm.fill(defaultSparql),
+        routes.ApiController.sparqlQuery))
+  }
+
+  def sparqlQuery = userProfileAction { implicit userOpt => implicit request =>
+    Async {
+      rest.ApiDAO(userOpt).get("sparql", request.queryString, request.headers).map { r =>
+        Status(r.status)
+          .stream(Enumerator.fromStream(r.ahcResponse.getResponseBodyAsStream))
+          .as(r.ahcResponse.getContentType)
+      }
+    }
   }
 }
