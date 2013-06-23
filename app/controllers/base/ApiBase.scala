ccae123c324a8afa1b9bfb156203028cda6fa8b3
Mike Bryant <mikesname@gmail.com>
2013-06-24 00:31:30 +0100

    Tweak error handling

--- app/controllers/base/ApiBase.scala
+++ app/controllers/base/ApiBase.scala
@@ -4,7 +4,7 @@ import play.api.libs.concurrent.Execution.Implicits._
 import play.api.mvc.Controller
 import models.json.{ClientConvertable, RestReadable}
 
-import play.api.libs.json.Json
+import play.api.libs.json.{JsError, Json}
 import defines.EntityType
 
 /**
@@ -21,7 +21,7 @@ trait ApiBase[TM] extends Controller with ControllerHelpers with AuthController 
         res.right.map { tm =>
           tm.fold(
             invalid = { err =>
-              BadRequest(err.toString) // Should be 500!
+              BadRequest(JsError.toFlatJson(err)) // Should be 500!
             },
             valid = { item =>
               Ok(Json.toJson(item)(cw.clientFormat))
