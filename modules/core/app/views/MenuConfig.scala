cb6d1b24c299377068758a55ab738b0370889fd1
Mike Bryant <mikesname@gmail.com>
2013-07-10 08:22:54 +0100

    Cleanup

--- modules/core/app/views/MenuConfig.scala
+++ modules/core/app/views/MenuConfig.scala
@@ -8,6 +8,16 @@ import java.util
  */
 object MenuConfig {
 
-  val mainSection = new java.util.LinkedHashMap[String,String]()
-  val adminSection = new util.LinkedHashMap[String,String]()
+  import collection.JavaConverters._
+
+  // Ugh, cannot find a mutable sorted Scala collection, so using
+  // this Java LinkedHashMap with a converter
+  private val mainSection = new java.util.LinkedHashMap[String,String]()
+  private val adminSection = new util.LinkedHashMap[String,String]()
+
+  def putMain(key: String, url: String) = mainSection.put(key, url)
+  def putAdmin(key: String, url: String) = adminSection.put(key, url)
+
+  def main = mainSection.asScala
+  def admin = adminSection.asScala
 }
