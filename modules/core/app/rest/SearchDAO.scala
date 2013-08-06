faea71aadfa94701400622183c04c8a6a6292625
Mike Bryant <mikesname@gmail.com>
2013-08-06 21:13:51 +0100

    Formatting

--- modules/core/app/rest/SearchDAO.scala
+++ modules/core/app/rest/SearchDAO.scala
@@ -13,15 +13,13 @@ case class SearchDAO(userProfile: Option[UserProfile]) extends RestDAO {
   def requestUrl = "http://%s:%d/%s/entities".format(host, port, mount)
 
   def get[MT](id: String)(implicit rd: RestReadable[MT]): Future[Either[RestError, MT]] = {
-    WS.url(enc(requestUrl, id)).withHeaders(authHeaders.toSeq: _*)
-      .get.map { response =>
+    WS.url(enc(requestUrl, id)).withHeaders(authHeaders.toSeq: _*).get.map { response =>
       checkErrorAndParse(response)(rd.restReads)
     }
   }
 
   def list[MT](ids: Seq[String])(implicit rd: RestReadable[MT]): Future[Either[RestError, List[MT]]] = {
-    WS.url(requestUrl).withHeaders(authHeaders.toSeq: _*)
-      .post(Json.toJson(ids)).map { response =>
+    WS.url(requestUrl).withHeaders(authHeaders.toSeq: _*).post(Json.toJson(ids)).map { response =>
       checkError(response).right.map { r =>
         r.json.validate[List[MT]](Reads.list(rd.restReads)).fold(
           valid = { list => list },
