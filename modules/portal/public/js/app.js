2abc22d86211e9059b5f69665b798f5188c44443
Mike Bryant <mikesname@gmail.com>
2013-08-15 16:33:35 +0100

    Continue trying to figure out angular stuff with search.

--- modules/portal/public/js/app.js
+++ modules/portal/public/js/app.js
@@ -2,21 +2,21 @@ var Portal = angular.module('Portal', []).service("$search", [SearchService]);
 
 Portal.config(['$routeProvider', function($routeProvider) {
   $routeProvider
-      .when("/portal/profile/", {
+      .when("/profile", {
         templateUrl: ANGULAR_PARTIALS + "/profile.tpl.html",
         controller: ProfileCtrl
       })
-      .when("/portal/search/", {
+      .when("/search", {
         templateUrl: ANGULAR_PARTIALS + "/search.tpl.html",
         controller: SearchCtrl
       })
-      .when("/portal/item/:itemType/:itemId", {
+      .when("/item/:itemType/:itemId", {
         templateUrl: ANGULAR_PARTIALS + "/item.tpl.html",
         controller: ItemCtrl
       })
-      .otherwise({redirectTo: "/portal/search/"});
+      .otherwise({redirectTo: "/search"});
 }]).config(['$locationProvider', function($locationProvider) {
-  $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(false);
   $locationProvider.hashPrefix = "/portal";
 }]);
 
--- modules/portal/public/js/controllers.js
+++ modules/portal/public/js/controllers.js
@@ -2,28 +2,73 @@
 function SearchCtrl($scope, $rootScope, $search, $location, $http) {
   console.log("Loading searchctrl")
 
+  function filterParams(params) {
+    var filtered = {};
+    for (key in params) {
+      if (params.hasOwnProperty(key)) {
+        if (params[key] !== undefined && params[key] != null && params[key] != "") {
+          filtered[key] = params[key] + "";
+        }
+      }
+    }
+    return filtered;
+  }
+
+  $scope.nextPage = function() {
+    $scope.searchParams.page++;
+    $scope.doSearch();
+  }
+
+  $scope.prevPage = function() {
+    $scope.searchParams.page = Math.max(1, $scope.searchParams.page - 1);
+    $scope.doSearch();
+  }
+
+  $scope.isSelectedType = function(type) {
+    return $scope.searchParams.st.indexOf(type) != -1;
+  }
+
+  $scope.addTypeFilter = function(type) {
+    $scope.searchParams.st.push(type);
+    $scope.doSearch();
+  }
+
+  $scope.removeTypeFilter = function(type) {
+    $scope.searchParams.st.splice($scope.searchParams.st.indexOf(type), 1);
+    $scope.doSearch();
+  }
+
   $scope.doSearch = function() {
+    console.log("Setting search: ", filterParams($scope.searchParams))
+    // Setting location causes problems!
+    //$location.search(filterParams($scope.searchParams));
+    //$location.search({st: undefined})
     $http({url: getSearchUrl(), method: "GET", headers:{Accept: "application/json"}}).success(function(data) {
       $scope.results = data;
-      console.log("Got data: ", data);
     });
   };
 
-  $scope.updateUrl = function() {
-
-  }
-
   $scope.searchParams = {
     q: $location.search().q || "",
+    page: parseInt($location.search().page, 10) || 1,
     excludes: [],
-    facets: []
+    facets: [],
+    st: []
   };
+
 
   var getSearchUrl = function() {
     var url = jsRoutes.controllers.portal.Application.search().url;
-    if (!$scope.searchParams.q.trim() == "") {
-      url += "?q=" + $scope.searchParams.q.trim();
+    url += "?q=" + $scope.searchParams.q.trim();
+    if ($scope.searchParams.page > 1) {
+      url += "&page=" + $scope.searchParams.page;
     }
+    if ($scope.searchParams.st.length > 0) {
+      for (i in $scope.searchParams.st) {
+        url += "&st[]=" + $scope.searchParams.st[i];
+      }
+    }
+    console.log(url)
     return url;
   }
 
