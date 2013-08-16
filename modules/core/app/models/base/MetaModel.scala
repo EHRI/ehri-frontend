c9211cbf39b7baf1c55250c9dc7a7f121ef831b2
Mike Bryant <mikesname@gmail.com>
2013-08-16 11:29:02 +0100

    Impemenent showing external links. Need to rationalise this code A LOT.

--- modules/core/app/models/base/MetaModel.scala
+++ modules/core/app/models/base/MetaModel.scala
@@ -100,7 +100,7 @@ trait MetaModel[+T <: Model] extends AnyModel {
   override def toStringAbbr(implicit lang: Lang): String = toStringLang(lang)
 }
 
-trait DescribedMeta[TD <: Description, T <: Described[TD]] extends MetaModel[T] {
+trait DescribedMeta[+TD <: Description, +T <: Described[TD]] extends MetaModel[T] {
   def descriptions: List[TD] = model.descriptions
 }
 
