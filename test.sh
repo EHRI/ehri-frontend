52ca4d78431452521a4ac200a3bd60520e381d94
Mike Bryant <mikesname@gmail.com>
2013-09-13 16:27:01 +0100

    Add more specs to test script

--- test.sh
+++ test.sh
@@ -12,3 +12,5 @@ play "test-only test.APISpec"          && \
 play "test-only test.json.JsonFormatSpec" && \
 play "test-only test.CountryScopeIntegrationSpec" && \
 play "test-only test.SupervisorWorkerIntegrationSpec"
+play "test-only test.SearchSpec"
+play "test-only test.ApplicationSpec"
