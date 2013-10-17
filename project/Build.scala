6db2b3339d1f6770c2c0df41188b7378453f0387
Mike Bryant <mikesname@gmail.com>
2013-10-17 16:19:39 +0100

    Remove unneeded module deps

--- project/Build.scala
+++ project/Build.scala
@@ -106,8 +106,8 @@ object ApplicationBuild extends Build {
   lazy val portal = play.Project(
     appName + "-portal", appVersion, appDependencies, path = file("modules/portal"),
     settings = Defaults.defaultSettings ++ play.Project.intellijCommandSettings("SCALA")
-  ).settings(otherSettings:_*).dependsOn(core, archdesc, authorities, vocabs)
-    .aggregate(core, archdesc, authorities, vocabs)
+  ).settings(otherSettings:_*).dependsOn(core, annotation, linking)
+    .aggregate(core, annotation, linking)
 
   lazy val admin = play.Project(
     appName + "-admin", appVersion, appDependencies, path = file("modules/admin"),
