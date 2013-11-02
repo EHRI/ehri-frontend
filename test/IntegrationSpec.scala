f726fe96ec6380154e0b176df8d01cf32965a894
Mike Bryant <mikesname@gmail.com>
2013-11-02 16:18:01 +0000

    Add a trivial browser test

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -3,10 +3,8 @@ package test
 import org.specs2.mutable._
 
 import play.api.test._
-import play.api.test.Helpers._
-import play.api.i18n.Messages
 
-class IntegrationSpec extends Specification {
+class IntegrationSpec extends PlaySpecification {
 
   "Application" should {
 
@@ -15,11 +13,9 @@ class IntegrationSpec extends Specification {
     )
 
     // Cannot get this working property, still says url is on about:blank...
-    /*"run in a browser" in new WithBrowser(port = 3333, app = app) {
+    "handle 404s property" in new WithBrowser(port = 3333, app = app) {
       browser.goTo("http://localhost:3333")
-      browser.$("a#signin").click()
-      browser.url must equalTo("http://localhost:3333/login")
-    }*/
+    }
   }
 
 }
