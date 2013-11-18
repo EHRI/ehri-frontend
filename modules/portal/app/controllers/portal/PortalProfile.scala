ce1de0b37edb14090e9af1b2a9559f7d45dba9ac
Mike Bryant <mikesname@gmail.com>
2013-11-18 13:18:44 +0000

    Do away with crap Ajax profile editing and tidy views

--- modules/portal/app/controllers/portal/PortalProfile.scala
+++ modules/portal/app/controllers/portal/PortalProfile.scala
@@ -23,23 +23,19 @@ trait PortalProfile extends Update[UserProfileF,UserProfile] {
   val form = models.forms.UserProfileForm.form
 
   def profile = withUserAction.async { implicit user => implicit request =>
-    if (isAjax) {
-      Future.successful(Ok(views.html.p.profile.profileDetails(user)))
-    } else {
-      val params = PageParams.fromRequest(request)
-      for {
-        watchList <- backend.pageWatching(user.id, params)
-      } yield Ok(views.html.p.profile.profile(user, watchList))
-    }
+    val params = PageParams.fromRequest(request)
+    for {
+      watchList <- backend.pageWatching(user.id, params)
+    } yield Ok(views.html.p.profile.profile(watchList))
   }
 
   def updateProfile = withUserAction { implicit user => implicit request =>
     if (isAjax) {
       Ok(views.html.p.profile.editProfileForm(
-        user, form.fill(user.model), controllers.portal.routes.Portal.updateProfilePost))
+        form.fill(user.model), controllers.portal.routes.Portal.updateProfilePost))
     } else {
       Ok(views.html.p.profile.editProfile(
-        user, form.fill(user.model), controllers.portal.routes.Portal.updateProfilePost))
+        form.fill(user.model), controllers.portal.routes.Portal.updateProfilePost))
     }
   }
 
@@ -57,16 +53,12 @@ trait PortalProfile extends Update[UserProfileF,UserProfile] {
             BadRequest(errorForm.errorsAsJson)
           } else {
             BadRequest(views.html.p.profile.editProfile(
-                user, errorForm, controllers.portal.routes.Portal.updateProfilePost))
+                errorForm, controllers.portal.routes.Portal.updateProfilePost))
           }
         }
         case Right(item) => {
-          if (isAjax) {
-            Ok(views.html.p.profile.profileDetails(item))
-          } else {
-            Redirect(controllers.portal.routes.Portal.profile)
-              .flashing("success" -> Messages("confirmations.profileUpdated"))
-          }
+          Redirect(controllers.portal.routes.Portal.profile)
+            .flashing("success" -> Messages("confirmations.profileUpdated"))
         }
       }
     }
