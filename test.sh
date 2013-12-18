e32cdb94e9c37cfe4801fbf0630c890281925107
Mike Bryant <mikesname@gmail.com>
2013-12-18 20:26:50 +0000

    Make it possible to run test-only via test script

--- test.sh
+++ test.sh
@@ -2,4 +2,8 @@
 
 export _JAVA_OPTIONS="-Xms64m -Xmx1024m -Xss2m -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M -Dconfig.file=conf/test.conf"
 
-play test
+if [ -z "$1" ]; then
+    play test
+else 
+    play "test-only $@"
+fi
