056fe84ce6f939fea1394924551c2517db310a0d
thibault <thibault@thibault-VirtualBox.(none)>
2014-01-29 20:00:41 +0000

    Admin interface to BS3. Include improvement of AngularJS APP (From BootStrapUI 0.2.0 to 0.10.0

--- conf/db_migrations/account_verification_100114.sql
+++ conf/db_migrations/account_verification_100114.sql
@@ -6,8 +6,8 @@
 START TRANSACTION;
 CREATE TABLE oauth2_association (
   id            VARCHAR(50) NOT NULL,
-  provider_id   VARCHAR(100) NOT NULL,
-  provider      VARCHAR(10) NOT NULL,
+  provider_id   VARCHAR(255) NOT NULL,
+  provider      VARCHAR(255) NOT NULL,
   created       TIMESTAMP NOT NULL,
   PRIMARY KEY (id, provider_id, provider)
 );
@@ -19,6 +19,5 @@ ALTER TABLE oauth2_association ADD CONSTRAINT oauth2_association_id FOREIGN KEY 
   */
 
 ALTER TABLE users ADD COLUMN verified BOOLEAN NOT NULL DEFAULT FALSE AFTER email;
-UPDATE users SET verified = 1;
 ALTER TABLE token ADD COLUMN is_sign_up BOOLEAN NOT NULL DEFAULT FALSE AFTER expires;
 COMMIT;
