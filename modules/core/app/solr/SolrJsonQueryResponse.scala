0a2181cee0ab43b95adf440f8933841c666534b2
Mike Bryant <mikesname@gmail.com>
2014-02-10 12:53:06 +0000

    Inital sketch of JSON query parser, not 100% implemented

---
+++ modules/core/app/solr/SolrJsonQueryResponse.scala
@@ -0,0 +1,138 @@
+package solr
+
+import utils.search._
+import play.api.libs.json.{Reads, JsObject, Json, JsValue}
+import defines.EntityType
+import utils.search.SearchHit
+import play.api.libs.json.JsObject
+import solr.facet.{SolrQueryFacet, QueryFacetClass, SolrFieldFacet, FieldFacetClass}
+import play.api.Logger
+
+object SolrJsonQueryResponse extends ResponseParser {
+  def apply(responseString: String) = new SolrJsonQueryResponse(Json.parse(responseString))
+}
+
+/**
+ * @author Mike Bryant (http://github.com/mikesname)
+ */
+case class SolrJsonQueryResponse(response: JsValue) extends QueryResponse {
+
+  import SolrConstants._
+  import play.api.libs.json._
+  import play.api.libs.functional.syntax._
+
+  case class SolrData(
+    count: Int,
+    rawDocs: Seq[JsObject],
+    highLights: Option[Map[String,Map[String,Seq[String]]]],
+    rawFacets: Map[String,Map[String,JsValue]]
+  )
+
+  object SolrData {
+
+    implicit val reads: Reads[SolrData] = (
+      (__ \ "grouped" \ ITEM_ID \ "matches").read[Int] and
+      (__ \ "grouped" \ ITEM_ID \ "doclist" \ "docs").read[Seq[JsObject]] and
+      (__ \ "highlighting").readNullable[Map[String,Map[String,Seq[String]]]] and
+      (__ \ "facet_counts").read[Map[String,Map[String,JsValue]]]
+    )(SolrData.apply _)
+  }
+
+  private def hitBuilder(id: String, itemId: String, name: String, entityType: EntityType.Value, gid: Long): SearchHit
+      = new SearchHit(id, itemId, name, entityType, gid)
+
+  private def hitReads: Reads[SearchHit] = (
+    (__ \ ID).read[String] and
+    (__ \ ITEM_ID).read[String] and
+    (__ \ NAME_EXACT).read[String] and
+    (__ \ TYPE).read[EntityType.Value] and
+    (__ \ "gid").read[Long]
+  )(hitBuilder _)
+
+
+  private lazy val raw: SolrData = response.as[SolrData]
+  private lazy val rawFieldFacets: Map[String,JsValue] = raw.rawFacets.get("facet_fields").getOrElse(Map.empty)
+  private lazy val rawQueryFacets: Map[String,JsValue] = raw.rawFacets.get("facet_queries").getOrElse(Map.empty)
+
+  def phrases: Seq[String] = Seq.empty
+
+  def items: Seq[SearchHit] = raw.rawDocs.flatMap { jsObj =>
+    jsObj.validate(hitReads).asOpt.map { hit =>
+      val highlights: Map[String,Seq[String]] = (for {
+        hl <- raw.highLights
+        fhl <- hl.get(hit.id)
+      } yield fhl).getOrElse(Map.empty)
+
+      val fields = jsObj.value.map { case (field,js) =>
+        field -> js.validate[String]
+      }.collect {
+        case (f, JsSuccess(v, p)) => f -> v
+      }.toMap
+
+      hit.copy(fields = fields, highlights = highlights)
+    }
+  }
+
+  private val fieldFacetValueReader: Reads[List[(String,Int)]] = {
+    __.read[List[JsValue]].map {
+      list =>
+        list.grouped(2).flatMap {
+          case item :: count :: Nil => {
+            Some((item.as[String], count.as[Int]))
+          }
+          case _ => Nil
+        }.toList
+    }
+  }
+
+  private def tagFunc(tags: List[String]): String = tags match {
+    case Nil => ""
+    case _ => "{!ex=" + tags.mkString(",") + "}"
+  }
+
+  private def extractFieldFacet(fc: FieldFacetClass, appliedFacets: List[AppliedFacet], tags: List[String] = Nil): Option[FacetClass[Facet]] = {
+    val applied: List[String] = appliedFacets.find(_.name == fc.key).map(_.values).getOrElse(List.empty[String])
+    rawFieldFacets.get(fc.key).flatMap { jsval =>
+      jsval.validate(fieldFacetValueReader).fold(
+        err => None,
+        fields => {
+          val facets = fields.map { case (text, count) =>
+            SolrFieldFacet(
+              text, text, None,
+              count, applied.contains(text))
+          }.toList
+          Some(fc.copy(facets = facets))
+        }
+      )
+    }
+  }
+
+  private def extractQueryFacet(fc: QueryFacetClass, appliedFacets: List[AppliedFacet], tags: List[String] = Nil): Option[FacetClass[Facet]] = {
+    val applied: List[String] = appliedFacets.find(_.name == fc.key).map(_.values).getOrElse(List.empty[String])
+    val facetsWithCount: List[SolrQueryFacet] = fc.facets.flatMap { qf =>
+      val nameValue = s"${tagFunc(tags)}${fc.key}:${qf.solrValue}"
+      rawQueryFacets.get(nameValue).map { v =>
+        qf.copy(count = v.as[Int], applied = applied.contains(qf.value))
+      }
+    }
+    Some(fc.copy(facets = facetsWithCount.toList))
+  }
+
+  def extractFacetData(appliedFacets: List[AppliedFacet], allFacets: utils.search.FacetClassList): utils.search.FacetClassList = {
+    val tags = allFacets.filter(_.tagExclude).map(_.key)
+    allFacets.flatMap {
+      case ffc: FieldFacetClass => extractFieldFacet(ffc, appliedFacets, tags)
+      case qfc: QueryFacetClass => extractQueryFacet(qfc, appliedFacets, tags)
+      case e => {
+        Logger.logger.warn("Unknown facet class type: {}", e)
+        None
+      }
+    }
+  }
+
+  def count: Int = raw.count
+
+  def highlightMap: Map[String, Map[String, Seq[String]]] = raw.highLights.getOrElse(Map.empty)
+
+  def spellcheckSuggestion: Option[(String, String)] = None
+}
