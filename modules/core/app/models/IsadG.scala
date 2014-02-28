fbbb17bf379eab6f0a80840b9730225872e60635
Mike Bryant <mikesname@gmail.com>
2014-02-28 09:55:51 +0100

    Add more levels of description

--- modules/core/app/models/IsadG.scala
+++ modules/core/app/models/IsadG.scala
@@ -6,9 +6,12 @@ case object IsadG {
 
   object LevelOfDescription extends Enumeration {
     type Type = Value
-    val Collection = Value("collection")
     val Fonds = Value("fonds")
     val Subfonds = Value("subfonds")
+    val Collection = Value("collection")
+    val Subcollection = Value("subcollection")
+    val RecordGroup = Value("recordGroup")
+    val SubRecordGroup =Value("subrecordGroup")
     val Series = Value("series")
     val Subseries = Value("subseries")
     val File = Value("file")
