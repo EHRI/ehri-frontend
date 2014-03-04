404266bfbeea9429422076af9e1f4f854c03b9ec
Mike Bryant <mikesname@gmail.com>
2014-03-04 15:13:46 +0000

    Attempting to change an item you can't access now throws a 404

--- test/integration/DocUnitViewsSpec.scala
+++ test/integration/DocUnitViewsSpec.scala
@@ -5,7 +5,7 @@ import models._
 import play.api.i18n.Messages
 import play.api.test.FakeRequest
 import play.api.http.{MimeTypes, HeaderNames}
-import backend.rest.PermissionDenied
+import backend.rest.ItemNotFound
 
 
 class DocUnitViewsSpec extends Neo4jRunnerSpec(classOf[DocUnitViewsSpec]) {
@@ -91,7 +91,7 @@ class DocUnitViewsSpec extends Neo4jRunnerSpec(classOf[DocUnitViewsSpec]) {
     "deny access to c2 when logged in as an ordinary user" in new FakeApp {
       val show = route(fakeLoggedInHtmlRequest(unprivilegedUser, GET,
           controllers.archdesc.routes.DocumentaryUnits.get("c2").url)).get
-      status(show) must throwA[PermissionDenied]
+      status(show) must throwA[ItemNotFound]
     }
 
     "allow deleting c4 when logged in" in new FakeApp {
