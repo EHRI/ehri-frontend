74689f70236b83e894737554de162f1d845a7252
Mike Bryant <mikesname@gmail.com>
2014-04-14 12:30:48 +0100

    Dubious work on search highlight, including fix for #200.
     - make it explicit that we're passing in and using HTML (which should
       make it more apparent how dubious the whole find/replace scheme is.)
     - exclude programmatic fields (like id, itemId, gid etc) from being
       highlighted.

--- modules/core/app/utils/search/SearchHit.scala
+++ modules/core/app/utils/search/SearchHit.scala
@@ -4,6 +4,7 @@ import defines.EntityType
 import scala.annotation.tailrec
 import scala._
 import solr.SolrConstants._
+import play.api.templates.Html
 
 /**
  * User: michaelb
@@ -20,12 +21,27 @@ case class SearchHit(
 ) {
 
   private def stripTags(value: String): String
-    = value.replaceAll("""<(?!\/?a(?=>|\s.*>))\/?.*?>""", "")
+    = value.replaceAll("""<(?!\/?(?=>|\s.*>))\/?.*?>""", "")
+
+  /**
+   * Filter the highlight fields for those that are used primarily for
+   * programmatic purposes. This is to prevent us highlighting things
+   * like IDs in the rare occasions when they are used as search queries.
+   *
+   * FIXME: This whole highlighting stuff is too complex and fragile since
+   * we are trying to highlight ad-hoc HTML. There's just too much stuff that
+   * can/will go wrong.
+   */
+  private lazy val filteredHighlights = highlights.filterNot { case (k, _) =>
+    k == ID || k == ITEM_ID || k == DB_ID || k == TYPE
+  }
+
+  private lazy val highlightFields = filteredHighlights.values.flatten.toList
 
   def bestHitText: Seq[String] = {
-    val handledFields = Seq(ID, ITEM_ID, NAME_EXACT, TYPE, DB_ID, NAME_MATCH)
+    val handledFields = Seq(ID, ITEM_ID, DB_ID, NAME_EXACT, TYPE, NAME_MATCH)
     (for {
-      (hitField, hits) <- highlights
+      (hitField, hits) <- filteredHighlights
       (field, text) <- fields if field == hitField && !handledFields.contains(field)
       firstHit <- hits.headOption
     } yield text.replace(stripTags(firstHit), firstHit)).toSeq
@@ -36,18 +52,18 @@ case class SearchHit(
    * @param text Some input text
    * @return  The text and a boolean indicating if highlighting was successful
    */
-  def highlight(text: String): (String, Boolean) = {
-    def canHighlightWith(raw: String, text: String): Option[String] = {
+  def highlight(text: Html): (Html, Boolean) = {
+    def canHighlightWith(raw: String, text: Html): Option[Html] = {
       val stripped = stripTags(raw)
-      if (text.contains(stripped)) Some(text.replace(stripped, raw)) else None
+      if (text.body.contains(stripped)) Some(Html(text.body.replace(stripped, raw))) else None
     }
-    @tailrec def tryHighlight(texts: List[String], input: String, ok: Boolean): (String, Boolean) = texts match {
+    @tailrec def tryHighlight(texts: Seq[String], input: Html, ok: Boolean): (Html, Boolean) = texts match {
       case t :: rest => canHighlightWith(t, text) match {
-        case rep@Some(replace) => tryHighlight(rest, replace, true)
+        case rep@Some(replace) => tryHighlight(rest, replace, ok = true)
         case None => tryHighlight(rest, input, ok)
       }
       case Nil => (input,ok)
     }
-    tryHighlight(highlights.values.flatten.toList, text, false)
+    tryHighlight(highlightFields, text, ok = false)
   }
 }
