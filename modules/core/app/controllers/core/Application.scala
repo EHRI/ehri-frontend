c52cdf16a392290ead64f6440b68f78199eca929
Mike Bryant <mikesname@gmail.com>
2014-06-25 16:35:39 +0100

    Cache some of the page global data we send to the client

--- modules/core/app/controllers/core/Application.scala
+++ modules/core/app/controllers/core/Application.scala
@@ -1,19 +1,15 @@
 package controllers.core
 
-import play.api.libs.concurrent.Execution.Implicits._
 import controllers.base.{AuthController, AuthConfigImpl}
-import models.base.AnyModel
-import models.json.RestReadable
 import play.api.mvc._
 import jp.t2v.lab.play2.auth.AsyncAuth
-import play.api.libs.json.Json
-import defines.EntityType
 import com.google.inject._
 import play.api.http.{MimeTypes, ContentTypes}
 import backend.Backend
-import backend.rest.SearchDAO
 import models.AccountDAO
 import play.api.Routes
+import play.api.cache.Cached
+import play.api.Play.current
 
 case class Application @Inject()(implicit globalConfig: global.GlobalConfig, backend: Backend, userDAO: AccountDAO) extends Controller with AsyncAuth with AuthConfigImpl with AuthController {
 
@@ -28,45 +24,49 @@ case class Application @Inject()(implicit globalConfig: global.GlobalConfig, bac
     MovedPermanently("/" + path + query)
   }
 
-  def jsRoutes = Action { implicit request =>
-    Ok(
-      Routes.javascriptRouter("jsRoutes")(
-        controllers.core.routes.javascript.SearchFilter.filter
-      )
-    ).as(MimeTypes.JAVASCRIPT)
+  def jsRoutes = Cached.status(_ => "pages:filterJsRoutes", OK, 3600) {
+    Action { implicit request =>
+      Ok(
+        Routes.javascriptRouter("jsRoutes")(
+          controllers.core.routes.javascript.SearchFilter.filter
+        )
+      ).as(MimeTypes.JAVASCRIPT)
+    }
   }
 
-  def localeData(lang: String) = Action { request =>
-    implicit val locale = play.api.i18n.Lang(lang)
+  def localeData(lang: String) = Cached.status(_ => "pages:localeData", OK, 3600) {
+    Action { request =>
+      implicit val locale = play.api.i18n.Lang(lang)
 
-    val js =
-      """
-        |var __languageData = {
-        |  %s
-        |};
-        |
-        |var __countryData = {
-        |  %s
-        |};
-        |
-        |var LocaleData = {
-        |  languageCodeToName: function(code) {
-        |    return __languageData[code] || code;
-        |  },
-        |  countryCodeToName: function(code) {
-        |    return __countryData[code] || code;
-        |  },
-        |}
-      """.stripMargin.format(
-        utils.i18n.languagePairList.map{ case (code, name) =>
-          code + ": \"" + name + "\""
-        }.mkString(",\n  "),
-        utils.i18n.countryPairList.map{ case (code, name) =>
-          code.toLowerCase + ": \"" + name + "\""
-        }.mkString(",\n  ")
-      )
+      val js =
+        """
+          |var __languageData = {
+          |  %s
+          |};
+          |
+          |var __countryData = {
+          |  %s
+          |};
+          |
+          |var LocaleData = {
+          |  languageCodeToName: function(code) {
+          |    return __languageData[code] || code;
+          |  },
+          |  countryCodeToName: function(code) {
+          |    return __countryData[code] || code;
+          |  },
+          |}
+        """.stripMargin.format(
+          utils.i18n.languagePairList.map{ case (code, name) =>
+            code + ": \"" + name + "\""
+          }.mkString(",\n  "),
+          utils.i18n.countryPairList.map{ case (code, name) =>
+            code.toLowerCase + ": \"" + name + "\""
+          }.mkString(",\n  ")
+        )
 
-    Ok(js).as(ContentTypes.JAVASCRIPT)
+      Ok(js).as(ContentTypes.JAVASCRIPT)
+    }
   }
 }
 
--- modules/portal/app/controllers/portal/PortalData.scala
+++ modules/portal/app/controllers/portal/PortalData.scala
@@ -3,6 +3,8 @@ package controllers.portal
 import play.api.http.MimeTypes
 import play.api.Routes
 import play.api.mvc.{Controller,Action}
+import play.api.cache.Cached
+import play.api.Play.current
 
 
 /**
@@ -10,51 +12,54 @@ import play.api.mvc.{Controller,Action}
  */
 object PortalData extends Controller {
 
-
-  def jsRoutes = Action { implicit request =>
-    Ok(
-      Routes.javascriptRouter("jsRoutes")(
-        controllers.portal.routes.javascript.Profile.watchItemPost,
-        controllers.portal.routes.javascript.Profile.unwatchItemPost,
-        controllers.portal.routes.javascript.Profile.updatePrefs,
-        controllers.portal.routes.javascript.Profile.profile,
-        controllers.portal.routes.javascript.Profile.updateProfile,
-        controllers.portal.routes.javascript.Profile.updateProfilePost,
-        controllers.portal.routes.javascript.Social.personalisedActivity,
-        controllers.portal.routes.javascript.Social.followUserPost,
-        controllers.portal.routes.javascript.Social.unfollowUserPost,
-        controllers.portal.routes.javascript.Social.followersForUser,
-        controllers.portal.routes.javascript.Social.blockUserPost,
-        controllers.portal.routes.javascript.Social.unblockUserPost,
-        controllers.portal.routes.javascript.Social.sendMessage,
-        controllers.portal.routes.javascript.Annotations.annotate,
-        controllers.portal.routes.javascript.Annotations.annotatePost,
-        controllers.portal.routes.javascript.Annotations.annotateField,
-        controllers.portal.routes.javascript.Annotations.annotateFieldPost,
-        controllers.portal.routes.javascript.Annotations.editAnnotation,
-        controllers.portal.routes.javascript.Annotations.editAnnotationPost,
-        controllers.portal.routes.javascript.Annotations.deleteAnnotation,
-        controllers.portal.routes.javascript.Annotations.deleteAnnotationPost,
-        controllers.portal.routes.javascript.Annotations.setAnnotationVisibilityPost,
-        controllers.core.routes.javascript.SearchFilter.filter,
-        controllers.portal.routes.javascript.Portal.browseItem
-      )
-    ).as(MimeTypes.JAVASCRIPT)
+  def jsRoutes = Cached.status(_ => "pages:portalJsRoutes", OK, 3600) {
+    Action { implicit request =>
+      Ok(
+        Routes.javascriptRouter("jsRoutes")(
+          controllers.portal.routes.javascript.Profile.watchItemPost,
+          controllers.portal.routes.javascript.Profile.unwatchItemPost,
+          controllers.portal.routes.javascript.Profile.updatePrefs,
+          controllers.portal.routes.javascript.Profile.profile,
+          controllers.portal.routes.javascript.Profile.updateProfile,
+          controllers.portal.routes.javascript.Profile.updateProfilePost,
+          controllers.portal.routes.javascript.Social.personalisedActivity,
+          controllers.portal.routes.javascript.Social.followUserPost,
+          controllers.portal.routes.javascript.Social.unfollowUserPost,
+          controllers.portal.routes.javascript.Social.followersForUser,
+          controllers.portal.routes.javascript.Social.blockUserPost,
+          controllers.portal.routes.javascript.Social.unblockUserPost,
+          controllers.portal.routes.javascript.Social.sendMessage,
+          controllers.portal.routes.javascript.Annotations.annotate,
+          controllers.portal.routes.javascript.Annotations.annotatePost,
+          controllers.portal.routes.javascript.Annotations.annotateField,
+          controllers.portal.routes.javascript.Annotations.annotateFieldPost,
+          controllers.portal.routes.javascript.Annotations.editAnnotation,
+          controllers.portal.routes.javascript.Annotations.editAnnotationPost,
+          controllers.portal.routes.javascript.Annotations.deleteAnnotation,
+          controllers.portal.routes.javascript.Annotations.deleteAnnotationPost,
+          controllers.portal.routes.javascript.Annotations.setAnnotationVisibilityPost,
+          controllers.core.routes.javascript.SearchFilter.filter,
+          controllers.portal.routes.javascript.Portal.browseItem
+        )
+      ).as(MimeTypes.JAVASCRIPT)
+    }
   }
 
   /**
    * Render entity types into the view to serve as JS constants.
    * @return
    */
-  def globalData = Action { implicit request =>
-    import defines.EntityType
-    Ok(
-      """
-        |var EntityTypes = {
-        |%s
-        |};
-      """.stripMargin.format(
-        "\t" + EntityType.values.map(et => s"$et: '$et'").mkString(",\n\t"))
-    ).as(MimeTypes.JAVASCRIPT)
+  def globalData = Cached.status(_ => "pages:globalData", OK, 3600) {
+    Action { implicit request =>
+      import defines.EntityType
+      Ok(
+        """
+          |var EntityTypes = {
+          |%s
+          |};
+        """.stripMargin.format(
+          "\t" + EntityType.values.map(et => s"$et: '$et'").mkString(",\n\t"))
+      ).as(MimeTypes.JAVASCRIPT)
+    }
   }
 }
