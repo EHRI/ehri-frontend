3192a333598cb57ca970ae68e1f28a965b73a833
Thibault Clérice <leponteineptique@gmail.com>
2014-06-27 11:06:26 +0200

    Revert "Fixed double repo in repo/items"
    This reverts commit 4c926839d0117aaaf58310b35f45ac21076e7ca7.

--- modules/portal/app/controllers/portal/FacetConfig.scala
+++ modules/portal/app/controllers/portal/FacetConfig.scala
@@ -219,7 +219,15 @@ trait FacetConfig extends Search {
   }
 
   protected val docSearchRepositoryFacets: FacetBuilder = { implicit request =>
-    docSearchFacets(request)
+    docSearchFacets(request) ++ List(
+      FieldFacetClass(
+        key="holderName",
+        name=Messages("documentaryUnit.heldBy"),
+        param="holder",
+        sort = FacetSort.Name,
+        display = FacetDisplay.DropDown
+      )
+    )
   }
 
   protected val conceptFacets: FacetBuilder = { implicit request =>
