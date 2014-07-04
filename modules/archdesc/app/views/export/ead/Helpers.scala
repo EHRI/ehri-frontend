c4d1c569c6930bd1a4f7939de6a133a5e8bc315a
Mike Bryant <mikesname@gmail.com>
2014-07-04 12:13:17 +0100

    More EAD integration, still highly dubious

--- modules/archdesc/app/views/export/ead/Helpers.scala
+++ modules/archdesc/app/views/export/ead/Helpers.scala
@@ -3,6 +3,7 @@ package views.export.ead
 import javax.xml.transform.{OutputKeys, TransformerFactory}
 import javax.xml.transform.stream.{StreamSource, StreamResult}
 import java.io.{StringReader, StringWriter}
+import org.pegdown.{LinkRenderer, Extensions, PegDownProcessor}
 
 /**
  * @author Mike Bryant (http://github.com/mikesname)
@@ -11,4 +12,28 @@ object Helpers {
   def tidyXml(xml: String): String = {
     XmlFormatter.format(xml)
   }
+
+  // Initialize Markdown processor for rendering markdown. NB: The
+  // instance is apparently not thread safe, so using a threadlocal
+  // here to be on the safe side.
+  private val markdownParser = new ThreadLocal[PegDownProcessor]
+  def getMarkdownProcessor = {
+    // NB: Eventually we want auto-linking. However this seems
+    // to crash pegdown at the moment.
+    //import org.pegdown.{Extensions,Parser,PegDownProcessor}
+    //val pegdownParser = new Parser(Extensions.AUTOLINKS)
+    //new PegDownProcessor//(pegdownParser)
+    Option(markdownParser.get).getOrElse {
+      val parser = new PegDownProcessor(Extensions.AUTOLINKS)
+      markdownParser.set(parser)
+      parser
+    }
+  }
+
+  private val eadSerializer = new ToEadSerializer(new LinkRenderer)
+
+  def renderMarkdownAsEad(text: String): String =
+    eadSerializer.toEad(getMarkdownProcessor.parseMarkdown(
+      getMarkdownProcessor.prepareSource(text.toCharArray)))
+
 }
