ae7b312ce592a03f0da631f1fd90f584f32329e5
Thibault Clérice <leponteineptique@gmail.com>
2014-08-18 11:03:49 +0200

    Lot of JS optimization

--- modules/portal/app/assets/js/portal-guide.js
+++ modules/portal/app/assets/js/portal-guide.js
@@ -1,157 +1,32 @@
-
-//From stackoverflow
-var urlParams;
-(window.onpopstate = function () {
-    var match,
-        pl     = /\+/g,  // Regex for replacing addition symbol with a space
-        search = /([^&=]+)=?([^&]*)/g,
-        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
-        query  = window.location.search.substring(1);
-
-    urlParams = {};
-    while (match = search.exec(query))
-       urlParams[decode(match[1])] = decode(match[2]);
-})();
-
 $(document).ready(function() {
+	$("#main").on('click', ".popover-accesspoints", function() {
+		var e=$(this);
+		if(e.data("loaded")) {
+			return true;
+		}
+		e.data("loaded", true)
 
-      // Handling form-submission via links, i.e. search form
-      // when facets are clicked
-      $('.facet-form').on("change", "input[type='checkbox']", function (e) {
-        $(e.target).closest("form").submit();
-      });
+		e.unbind('click');
+		e.css("cursor", "wait");
+		$.get(jsRoutes.controllers.portal.Portal.linkedDataInContext(e.attr("data-target"), VIRTUAL_UNIT).url,function(d){
+			var links = []
+			if(d.length > 0) {
+				$.each(d, function(index, link) {
+							links.push('<li><a href="'+ jsRoutes.controllers.portal.Guides.browseDocument(GUIDE_PATH, link.id).url +'">'+ link.name+'</a></li>')
+						})
 
-    //Tooltip
-    $('.facet-form').tooltip({
-        selector : '[data-toggle="tooltip"]',
-        container : 'body'
-    });
-    $(".facet-form h1 .search").on("click", function(e) {
-        e.preventDefault();
-        cross = $(this).children(".glyphicon");
+				var html = '<small><ul class="list-unstyled">' + links.join(" ") + '<li>';
+				if(links.length == 5) { var html = html + '<a href="'+ VIRTUAL_UNIT_ROUTE + e.attr("data-target") + '">' + (parseInt(e.attr("data-count")) - d.length) + ' more ...</a></li>'; }
+				var html = html + '</ul></small>';
+				e.popover({content: html, html: true, trigger: "click", container : "body"}).popover('show');
+				e.css("cursor", "pointer");
+			} else {
 
-        if(cross.hasClass("glyphicon-search")) {
-            $(this).parents(".facet-form").children(".facet-search").show();
-            cross.removeClass("glyphicon-search").addClass("glyphicon-remove");
-        } else {
-            $(this).parents(".facet-form").children(".facet-search").hide();
-            cross.removeClass("glyphicon-remove").addClass("glyphicon-search");
-            $.get(
-                form.attr("data-target"),
-                {},
-                function(data) {
-                    form.children("ul.facet-list").html(data).show();
-                    if(cross.hasClass("glyphicon-plus-sign")) {
-                        cross.removeClass("glyphicon-plus-sign").addClass("glyphicon-minus-sign");
-                    }
-                },
-                "html"
-            );
-        }
-    });
-    $(".facet-form .facet-search button[type='submit']").on("click", function(e) {
-        e.preventDefault();
-        dat = $(this);
-        form = dat.parents(".facet-form");
-        Q = form.find("input.q").val();
-        $.get(
-            form.attr("data-target") + "?q=" + Q,
-            {},
-            function(data) {
-                form.children("ul.facet-list").html(data).show();
-            },
-            "html"
-        );
-    });
-               
-    $(".facet-form h1 .more").on("click", function(e) {
-        e.preventDefault();
-        dat = $(this);
-        cross = dat.children(".glyphicon");
-        form = dat.parents(".facet-form");
-        if(form.find("li.facet").length == 0) {
-            $.get(
-                form.attr("data-target"),
-                {},
-                function(data) {
-                    form.children("ul.facet-list").html(data).show();
-                    if(cross.hasClass("glyphicon-plus-sign")) {
-                        cross.removeClass("glyphicon-plus-sign").addClass("glyphicon-minus-sign");
-                    }
-                },
-                "html"
-            );
-        } else {
-            form.children("ul.facet-list").toggle();
-            if(cross.hasClass("glyphicon-plus-sign")) {
-                cross.removeClass("glyphicon-plus-sign").addClass("glyphicon-minus-sign");
-            } else {
-                cross.addClass("glyphicon-plus-sign").removeClass("glyphicon-minus-sign");
-            }
-        }
-    });
-    $(".facet-form").on("click", " ul.facet-list a[href]", function(e) {
-        e.preventDefault();
-        form = $(this);
-        list = form.parent().parent("li.facet").children("ul");
-        if(list.children("li").length > 0) {
-            list.toggle();
-        } else {
-            $.get(
-                form.attr("href"),
-                {},
-                function(data) {
-                    list.html(data);
-                },
-                "html"
-            );
-        }
-    });
-    $(".facet-form ul.facet-list").each(function() {
-        $(this).data("ajaxready", true).scroll(function() {
-            dat = $(this);
-            milestone = dat.children(".facet-scroll");
-
-            if($(this).data("ajaxready") == false || milestone.length == 0) {
-                return;
-            }
-
-            offset = milestone.position();
-
-            if(($(this).height() + 5) > offset.top) {
-                $(this).data("ajaxready", false);
-                milestone.show();
-                $.get(
-                    milestone.attr("data-target"),
-                    {},
-                    function(data) {
-                        milestone.remove();
-                        dat.append(data);
-                        if(dat.find(".facet-scroll") && dat.find(".facet-scroll").length == 1) {
-                            dat.data("ajaxready", true);
-                        }
-                    },
-                    "html"
-                );
-            }
-        });
-    });
-
-
-    $(".facet-form").each(function() {
-        var form = $(this),
-               $param = urlParams[form.attr("data-name")];
-        if(form.attr("data-name") in urlParams && $param != "") {
-            $(this).find(".search").trigger("click");
-            Q = form.find("input.q").val($param);
-            $.get(
-                form.attr("data-target") + "?q=" + $param,
-                {},
-                function(data) {
-                    form.children("ul.facet-list").html(data).show();
-                },
-                "html"
-            );
-        }
-    });
-});
+				if(e.find(".glyphicon-search").length > 0) {
+					e.hide();
+				}
+				e.css("cursor", "pointer");
+			}
+		});
+	});
+})
