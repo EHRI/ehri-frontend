813f4ae9f9608b09575cba1333130c2dd133e7b2
Thibault Clérice <leponteineptique@gmail.com>
2014-08-20 10:58:16 +0200

    Fix for closing / opeing popovers

--- modules/portal/app/assets/js/portal-guide.js
+++ modules/portal/app/assets/js/portal-guide.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
+	var resetPopover = function(elem) {
+		$("#main .popover-accesspoints").not(elem).each(function() {
+			var e = $(this);
+			if(e.data("loaded")) {
+				e.popover("hide");
+			}
+		})
+	}
 	$("#main").on('click', ".popover-accesspoints", function() {
-		var e=$(this);
+		var e=$(this),
+			that = this;
 		if(e.data("loaded")) {
 			return true;
 		}
@@ -12,13 +21,15 @@ $(document).ready(function() {
 			var links = []
 			if(d.length > 0) {
 				$.each(d, function(index, link) {
-							links.push('<li><a href="'+ jsRoutes.controllers.portal.Guides.browseDocument(GUIDE_PATH, link.id).url +'">'+ link.name+'</a></li>')
-						})
+					links.push('<li><a href="'+ jsRoutes.controllers.portal.Guides.browseDocument(GUIDE_PATH, link.id).url +'">'+ link.name+'</a></li>')
+				})
 
 				var html = '<small><ul class="list-unstyled">' + links.join(" ") + '<li>';
 				if(links.length == 5) { var html = html + '<a href="'+ VIRTUAL_UNIT_ROUTE + e.attr("data-target") + '">' + (parseInt(e.attr("data-count")) - d.length) + ' more ...</a></li>'; }
 				var html = html + '</ul></small>';
 				e.popover({content: html, html: true, trigger: "click", container : "body"}).popover('show');
+				resetPopover(that)
+				e.on("click", function() { resetPopover(this); });
 				e.css("cursor", "pointer");
 			} else {
 
