f9363f9154e56157786447542e9fe52fe219b3aa
Thibault Clérice <leponteineptique@gmail.com>
2014-09-03 16:56:20 +0200

    Multiple translations

--- modules/portal/app/assets/js/portal-guide.js
+++ modules/portal/app/assets/js/portal-guide.js
@@ -41,3 +41,76 @@ $(document).ready(function() {
 		});
 	});
 })
+
+jQuery(function($) {
+
+  var FB_REDIRECT_HASH = "#_=_";
+/*
+ * Description viewport code. This fixes a viewport to a list
+* of item descriptions so only the selected one is present
+* at any time.
+*/
+
+// HACK! If there's a description viewport, disable jumping
+// to the element on page load... this is soooo horrible.
+
+/**
+* Determine if the fragment refers to a description element.
+*/
+function isDescriptionRef(descId) {
+// NB: The _=_ is what Facebook adds to Oauth login redirects
+return descId
+    && descId != FB_REDIRECT_HASH
+    && $(descId).hasClass("description-holder");
+}
+
+setTimeout(function() {
+if (isDescriptionRef(location.hash)) {
+  window.scrollTo(0, 0);
+}
+}, 0);
+
+$(".description-switch").click(function(e) {
+	e.preventDefault();
+	var descId = "#desc-" + $(this).data("id");
+	location.hash = descId;
+	switchDescription(descId);
+});
+
+function switchDescription(descId) {
+$(".description-viewport").each(function(i, elem) {
+
+  var $vp = $(elem);
+  var $descs = $vp.find(".description-holder");
+
+  // If the hash isn't set, default to the first element
+  if (!descId) {
+    descId = "#" + $descs.first().attr("id");
+  }
+
+  var $theitem = $(descId, $vp);
+
+  $theitem.show();
+
+  $descs.not($theitem).hide();
+
+  // Set the active class on the current description
+  $(".description-switch[href='" + descId + "']").addClass("active")
+  $(".description-switch[href!='" + descId + "']").removeClass("active")
+});
+
+}
+
+function collapseDescriptions() {
+if (isDescriptionRef(location.hash)) {
+  switchDescription(location.hash);
+} else {
+  switchDescription();
+}
+}
+
+History.Adapter.bind(window, 'hashchange', collapseDescriptions);
+
+// Trigger a change on initial load...
+collapseDescriptions();
+})
