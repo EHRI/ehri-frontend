919b0ca64a29ed904e1511ddc8e3d96535447497
Mike Bryant <mikesname@gmail.com>
2014-10-21 14:26:57 +0100

    Retooling of search hit highlighting (fixes #407).
     - Highlighting no longer attempts to munge a whole chunk of Html,
       it now has to be applied individually to fields
     - Added a new interface for a `Highlighter` with a highlight method
       and made `SearchHit` implement this, which allows using a dummy
       highlighter when there's no hit available
     - Still a bit WIP...

--- modules/core/app/utils/search/SearchHit.scala
+++ modules/core/app/utils/search/SearchHit.scala
@@ -18,10 +18,14 @@ case class SearchHit(
   fields: Map[String,String] = Map.empty,
   highlights: Map[String,Seq[String]] = Map.empty,
   phrases: Seq[String] = Seq.empty
-) {
+) extends views.Highlighter {
 
   private def stripTags(value: String): String
     = value.replaceAll("""<(?!\/?(?=>|\s.*>))\/?.*?>""", "")
+
+  private val skipFields = Set(
+    ID, ITEM_ID, DB_ID, PARENT_ID, TYPE, LANGUAGE_CODE
+  )
 
   /**
    * Filter the highlight fields for those that are used primarily for
@@ -33,44 +37,36 @@ case class SearchHit(
    * can/will go wrong.
    */
   private lazy val filteredHighlights = highlights.filterNot { case (k, _) =>
-    k == ID || k == ITEM_ID || k == DB_ID || k == TYPE
+    skipFields.contains(k)
   }
 
-  private lazy val highlightFields = filteredHighlights.values.flatten.toList
-
-  def bestHitText: Seq[String] = {
-    val handledFields = Seq(ID, ITEM_ID, DB_ID, NAME_EXACT, TYPE, NAME_MATCH)
-    (for {
-      (hitField, hits) <- filteredHighlights
-      (field, text) <- fields if field == hitField && !handledFields.contains(field)
-      firstHit <- hits.headOption
-    } yield text.replace(stripTags(firstHit), firstHit)).toSeq
-  }
+  private lazy val highlightFields: List[String] = filteredHighlights.values.flatten.toList
 
   /**
    * Try and highlight some text according to the highlights.
    * @param text Some input text
    * @return  The text and a boolean indicating if highlighting was successful
    */
-  def highlight(text: Html): (Html, Boolean) = {
-    def canHighlightWith(raw: String, html: Html): Either[Html,Html] = {
+  def highlight(text: String): Html = {
+    def canHighlightWith(raw: String, text: String): Either[String,String] = {
       // NB: We have to escape the input string using HtmlFormat because it's
       // specialised to avoid XSS. This assumes that the highlight material coming
       // back from Solr doesn't itself contain XSS attacks... what are the chances
       // this will come back to bite me?
       val stripped = HtmlFormat.escape(stripTags(raw)).body
-      if (html.body.contains(stripped)) Right(Html(html.body.replace(stripped, raw)))
-      else Left(html)
+      if (text.contains(stripped)) Right(text.replace(stripped, raw))
+      else Left(text)
     }
-    @tailrec def tryHighlight(texts: Seq[String], input: Html, ok: Boolean): (Html, Boolean) = {
+    @tailrec def tryHighlight(texts: Seq[String], input: String, ok: Boolean): String = {
       texts match {
         case t :: rest => canHighlightWith(t.trim, input) match {
-          case Right(replace) => tryHighlight(rest, replace, ok = true)
+          case Right(replace) => replace
           case Left(last) => tryHighlight(rest, last, ok)
         }
-        case Nil => (input,ok)
+        case Nil => input
       }
     }
-    tryHighlight(highlightFields, text, ok = false)
+
+    Html(tryHighlight(highlightFields, text, ok = false))
   }
 }
