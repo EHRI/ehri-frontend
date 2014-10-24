2ef12844399b3cb9c7c12ed5929f259aca5c3032
Mike Bryant <mikesname@gmail.com>
2014-10-24 14:30:52 +0100

    Minor consistency fixes

--- test/helpers/TestConfiguration.scala
+++ test/helpers/TestConfiguration.scala
@@ -37,8 +37,7 @@ trait TestConfiguration {
   val feedbackBuffer = collection.mutable.HashMap.empty[Int,Feedback]
   val mailBuffer = collection.mutable.ListBuffer.empty[MockMail]
   val searchParamBuffer = collection.mutable.ListBuffer.empty[ParamLog]
-  val indexEventBuffer = collection.mutable.ArrayBuffer.empty[String]
-  val userDb = collection.mutable.HashMap.empty[String,Account]
+  val indexEventBuffer = collection.mutable.ListBuffer.empty[String]
 
   // Might want to mock the backend at at some point!
   def testBackend: Backend = new RestBackend(testEventHandler)
