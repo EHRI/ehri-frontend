ba68367dd966b52c21aa1b8fdca73e1ffea576dc
Mike Bryant <mikesname@gmail.com>
2015-01-15 09:44:00 +0000

    Expand Country object model, breaking single report field into more specific sections.
    Fixes #501.

--- modules/core/test/models/json/JsonFormatSpec.scala
+++ modules/core/test/models/json/JsonFormatSpec.scala
@@ -3,7 +3,6 @@ package models.json
 import play.api.libs.json.{JsValue, Json}
 import play.api.test.PlaySpecification
 import models._
-import scala.io.Source
 import defines.EntityType
 
 class JsonFormatSpec extends PlaySpecification {
@@ -14,16 +13,15 @@ class JsonFormatSpec extends PlaySpecification {
   "Documentary Unit Format should read and write with no changes" in {
     val validation = readResource(EntityType.DocumentaryUnit).validate[DocumentaryUnitF]
     // The JSON should parse correctly
-    validation.asEither must beRight
-    val doc = validation.get
+    validation.asEither must beRight.which { doc =>
+      // Writing the doc and reading it again should produce exactly
+      // the same object
+      doc mustEqual Json.toJson(doc).as[DocumentaryUnitF]
 
-    // Writing the doc and reading it again should produce exactly
-    // the same object
-    doc mustEqual Json.toJson(doc).as[DocumentaryUnitF]
-
-    // And error when parsing the wrong type
-    val badParse = readResource(EntityType.Repository).validate[DocumentaryUnitF]
-    badParse.asEither must beLeft
+      // And error when parsing the wrong type
+      val badParse = readResource(EntityType.Repository).validate[DocumentaryUnitF]
+      badParse.asEither must beLeft
+    }
   }
 
   "Documentary Unit metadata should read correctly" in {
@@ -36,20 +34,29 @@ class JsonFormatSpec extends PlaySpecification {
 
   "HistoricalAgent Format should read and write with no changes" in {
     val validation = readResource(EntityType.HistoricalAgent).validate[HistoricalAgentF]
-    validation.asEither must beRight
-    val actor = validation.get
-    actor mustEqual Json.toJson(actor).as[HistoricalAgentF]
-    val badParse = readResource(EntityType.Repository).validate[HistoricalAgentF]
-    badParse.asEither must beLeft
+    validation.asEither must beRight.which { actor =>
+      actor mustEqual Json.toJson(actor).as[HistoricalAgentF]
+      val badParse = readResource(EntityType.Repository).validate[HistoricalAgentF]
+      badParse.asEither must beLeft
+    }
   }
 
   "Repository Format should read and write with no changes" in {
     val validation = readResource(EntityType.Repository).validate[RepositoryF]
-    validation.asEither must beRight
-    val repo = validation.get
-    repo mustEqual Json.toJson(repo).as[RepositoryF]
-    val badParse = readResource(EntityType.HistoricalAgent).validate[RepositoryF]
-    badParse.asEither must beLeft
+    validation.asEither must beRight.which { repo =>
+      repo mustEqual Json.toJson(repo).as[RepositoryF]
+      val badParse = readResource(EntityType.HistoricalAgent).validate[RepositoryF]
+      badParse.asEither must beLeft
+    }
+  }
+
+  "Country Format should read and write with no changes" in {
+    val validation = readResource(EntityType.Country).validate[CountryF]
+    validation.asEither must beRight.which { country =>
+      country mustEqual Json.toJson(country).as[CountryF]
+      val badParse = readResource(EntityType.Repository).validate[CountryF]
+      badParse.asEither must beLeft
+    }
   }
 
   "Repository metadata should read correctly" in {
@@ -64,64 +71,64 @@ class JsonFormatSpec extends PlaySpecification {
 
   "Concept Format should read and write with no changes" in {
     val validation = readResource(EntityType.Concept).validate[ConceptF]
-    validation.asEither must beRight
-    val concept = validation.get
-    concept mustEqual Json.toJson(concept).as[ConceptF]
-    val badParse = readResource(EntityType.Repository).validate[ConceptF]
-    badParse.asEither must beLeft
+    validation.asEither must beRight.which { concept =>
+      concept mustEqual Json.toJson(concept).as[ConceptF]
+      val badParse = readResource(EntityType.Repository).validate[ConceptF]
+      badParse.asEither must beLeft
+    }
   }
 
   "Vocabulary Format should read and write with no changes" in {
     val validation = readResource(EntityType.Vocabulary).validate[VocabularyF]
-    validation.asEither must beRight
-    val vocabulary = validation.get
-    vocabulary mustEqual Json.toJson(vocabulary).as[VocabularyF]
-    val badParse = readResource(EntityType.Repository).validate[VocabularyF]
-    badParse.asEither must beLeft
+    validation.asEither must beRight.which { vocabulary =>
+      vocabulary mustEqual Json.toJson(vocabulary).as[VocabularyF]
+      val badParse = readResource(EntityType.Repository).validate[VocabularyF]
+      badParse.asEither must beLeft
+    }
   }
 
   "UserProfile Format should read and write with no changes" in {
     val validation = readResource(EntityType.UserProfile).validate[UserProfileF]
     // The JSON should parse correctly
-    validation.asEither must beRight
-    val user = validation.get
-    user mustEqual Json.toJson(user).as[UserProfileF]
-    val badParse = readResource(EntityType.Repository).validate[UserProfileF]
-    badParse.asEither must beLeft
+    validation.asEither must beRight.which { user =>
+      user mustEqual Json.toJson(user).as[UserProfileF]
+      val badParse = readResource(EntityType.Repository).validate[UserProfileF]
+      badParse.asEither must beLeft
+    }
   }
 
   "Group Format should read and write with no changes" in {
     val validation = readResource(EntityType.Group).validate[GroupF]
     // The JSON should parse correctly
-    validation.asEither must beRight
-    val group = validation.get
-    group mustEqual Json.toJson(group).as[GroupF]
-    val badParse = readResource(EntityType.Repository).validate[GroupF]
-    badParse.asEither must beLeft
+    validation.asEither must beRight.which { group =>
+      group mustEqual Json.toJson(group).as[GroupF]
+      val badParse = readResource(EntityType.Repository).validate[GroupF]
+      badParse.asEither must beLeft
+    }
   }
 
   "Annotation Format should read and write with no changes" in {
     val validation = readResource(EntityType.Annotation).validate[AnnotationF]
     // The JSON should parse correctly
-    validation.asEither must beRight
-    val annotation = validation.get
-    annotation mustEqual Json.toJson(annotation).as[AnnotationF]
-    val badParse = readResource(EntityType.Repository).validate[AnnotationF]
-    badParse.asEither must beLeft
+    validation.asEither must beRight.which { annotation =>
+      annotation mustEqual Json.toJson(annotation).as[AnnotationF]
+      val badParse = readResource(EntityType.Repository).validate[AnnotationF]
+      badParse.asEither must beLeft
+    }
   }
 
   "Virtual Unit Format should read correctly" in {
     val validation = readResource(EntityType.VirtualUnit).validate[VirtualUnit]
     // The JSON should parse correctly
-    validation.asEither must beRight
-    val virtualUnit = validation.get
-    virtualUnit.includedUnits.headOption must beSome
+    validation.asEither must beRight.which { virtualUnit =>
+      virtualUnit.includedUnits.headOption must beSome
 
-    val validation2 = readResource(EntityType.VirtualUnit.toString + "NoDesc.json").validate[VirtualUnit]
-    // The JSON should parse correctly
-    validation2.asEither must beRight
-    val virtualUnit2 = validation2.get
-    virtualUnit2.includedUnits.headOption must beNone
+      val validation2 = readResource(EntityType.VirtualUnit.toString + "NoDesc.json").validate[VirtualUnit]
+      // The JSON should parse correctly
+      validation2.asEither must beRight.which { virtualUnit2 =>
+        virtualUnit2.includedUnits.headOption must beNone
+      }
+    }
   }
 
   "Content type format should read and write correctly" in {
