e4ff00bd92deb581b2c52bf9c3648fe135ecfaba
Mike Bryant <mikesname@gmail.com>
2015-01-27 16:20:26 +0000

    Add a legacy field on accounts which modifies password checking.
     - Imported passwords are unsalted MD5 from Drupal, which are
       saved with BCrypt on import. When checking these we use the
       isLegacy account field to determine whether to MD5 the incoming
       pw before checking.
     - When a legacy account user logs in successfully, changes, or resets
       their password, the isLegacy flag is switched off.

--- modules/core/app/auth/HashedPassword.scala
+++ modules/core/app/auth/HashedPassword.scala
@@ -1,5 +1,6 @@
 package auth
 
+import org.apache.commons.codec.digest.DigestUtils
 import org.mindrot.jbcrypt.BCrypt
 
 /**
@@ -14,6 +15,13 @@ object HashedPassword {
 
 case class HashedPassword private(s: String) {
   def check(pw: String) =  BCrypt.checkpw(pw, s)
+
+  /**
+   * Legacy passwords from the Drupal 6 system were stored as
+   * unsalted MD5. These were BCrypted when imported so the
+   * incoming password needs MD5ing prior to checking.
+   */
+  def checkLegacy(pw: String) =  BCrypt.checkpw(DigestUtils.md5Hex(pw), s)
 
   override def toString = s
 }
