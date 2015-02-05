69b624f3867b4df8245c2742f1d19539d0448fbe
Mike Bryant <mikesname@gmail.com>
2015-02-05 11:20:28 +0000

    Fix linting issue by moving class out of package object

--- modules/backend/app/defines/package.scala
+++ modules/backend/app/defines/package.scala
@@ -4,41 +4,9 @@ import play.api.data.format.Formatter
 package object defines {
 
   import language.implicitConversions
-  implicit def enumToString(e: Enumeration#Value) = e.toString
+  implicit def enumToString(e: Enumeration#Value): String = e.toString
 
   import play.api.libs.json._
-
-  import play.api.mvc.PathBindable
-  import play.api.mvc.QueryStringBindable
-
-  abstract class BindableEnum extends Enumeration {
-    
-    implicit def bindableEnum = new PathBindable[Value] {
-      def bind(key: String, value: String) =
-        values.find(_.toString.toLowerCase == value.toLowerCase) match {
-          case Some(v) => Right(v)
-          case None => Left("Unknown url path segment '" + value + "'")
-        }
-      def unbind(key: String, value: Value) = value.toString.toLowerCase
-    }
-
-    implicit def queryStringBinder(implicit stringBinder: QueryStringBindable[String]) = new QueryStringBindable[Value] {
-      override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, Value]] = {
-        for {
-          v <- stringBinder.bind(key, params)
-        } yield {
-          v match {
-            case Right(p) if values.exists(_.toString.toLowerCase == p.toLowerCase) =>
-              Right(withName(p.toLowerCase))
-            case _ => Left("Unable to bind a valid value from alternatives: " + values)
-          }
-        }
-      }
-      override def unbind(key: String, value: Value): String = {
-        stringBinder.unbind(key, value)
-      }
-    }
-  }
 
   object EnumUtils {
 
