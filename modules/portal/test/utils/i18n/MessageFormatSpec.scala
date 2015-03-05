94b0fa7cc5cb210f123007a203de1fd66e6932cd
Mike Bryant <mikesname@gmail.com>
2015-03-05 10:32:27 +0000

    More translations (de, pl) and i18n fixes

---
+++ modules/portal/test/utils/i18n/MessageFormatSpec.scala
@@ -0,0 +1,13 @@
+package utils.i18n
+
+import play.api.test.{FakeApplication, PlaySpecification}
+
+/**
+ * Test various format-sensitive messages
+ *
+ * @author Mike Bryant (http://github.com/mikesname)
+ */
+class MessageFormatSpec extends PlaySpecification {
+
+  // TODO
+}
