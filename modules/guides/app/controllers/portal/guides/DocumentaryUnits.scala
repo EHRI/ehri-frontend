6f4b7d0fec331cfe74e61a7bcf41e43b5d94df65
Mike Bryant <mikesname@gmail.com>
2015-03-13 12:30:09 +0000

    Refactoring guide document view to display child items (fixes #561)
    Various other tweaks which hopefully won't break anything.

--- modules/guides/app/controllers/portal/guides/DocumentaryUnits.scala
+++ modules/guides/app/controllers/portal/guides/DocumentaryUnits.scala
@@ -3,29 +3,46 @@ package controllers.portal.guides
 import auth.AccountManager
 import backend.Backend
 import com.google.inject._
+import controllers.generic.SearchType
+import controllers.portal.FacetConfig
 import controllers.portal.base.{Generic, PortalController}
 import models.{Guide, GuidePage, _}
-import utils.search.{SearchItemResolver, SearchEngine}
+import utils.search.{SearchConstants, SearchItemResolver, SearchEngine}
 import views.html.p
 
+import play.api.libs.concurrent.Execution.Implicits._
 
 @Singleton
 case class DocumentaryUnits @Inject()(implicit globalConfig: global.GlobalConfig, searchEngine: SearchEngine, searchResolver: SearchItemResolver, backend: Backend,
                             accounts: AccountManager, pageRelocator: utils.MovedPageLookup)
   extends PortalController
-  with Generic[DocumentaryUnit] {
+  with Generic[DocumentaryUnit]
+  with SearchType[DocumentaryUnit]
+  with FacetConfig {
 
-  def browse(path: String, id: String) = GetItemAction(id).apply { implicit request =>
-    itemOr404(Guide.find(path, activeOnly = true)) { guide =>
-      Ok(p.guides.documentaryUnit(
-        guide,
-        GuidePage.document(Some(request.item.toStringLang)),
-        guide.findPages(),
-        request.item,
-        request.annotations,
-        request.links,
-        request.watched
-      ))
+  def browse(path: String, id: String) = GetItemAction(id).async { implicit request =>
+    futureItemOr404 {
+      Guide.find(path, activeOnly = true).map { guide =>
+        val filterKey = if (!hasActiveQuery(request)) SearchConstants.PARENT_ID
+          else SearchConstants.ANCESTOR_IDS
+
+        findType[DocumentaryUnit](
+          filters = Map(filterKey -> request.item.id),
+          facetBuilder = docSearchFacets
+        ).map { result =>
+          Ok(p.guides.documentaryUnit(
+            guide,
+            GuidePage.document(Some(request.item.toStringLang)),
+            guide.findPages(),
+            request.item,
+            result,
+            controllers.portal.guides.routes.DocumentaryUnits.browse(path, id),
+            request.annotations,
+            request.links,
+            request.watched
+          ))
+        }
+      }
     }
   }
 }
