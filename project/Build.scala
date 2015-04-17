29e5cc8e3b7d56bd92c44e47255344d5015ab74b
Mike Bryant <mikesname@gmail.com>
2015-04-17 19:45:45 +0100

    Update play2-auth to 0.13.2

--- project/Build.scala
+++ project/Build.scala
@@ -59,7 +59,7 @@ object ApplicationBuild extends Build {
     "com.google.inject" % "guice" % "4.0-beta",
 
     // Authentication
-    "jp.t2v" %% "play2-auth" % "0.13.0",
+    "jp.t2v" %% "play2-auth" % "0.13.2",
 
     // Password hashing
     "org.mindrot" % "jbcrypt" % "0.3m",
@@ -92,7 +92,7 @@ object ApplicationBuild extends Build {
   )
 
   val testDependencies = backendTestDependencies ++ Seq(
-    "jp.t2v" %% "play2-auth-test" % "0.13.0" % "test"
+    "jp.t2v" %% "play2-auth-test" % "0.13.2" % "test"
   )
 
   val additionalResolvers = Seq(
