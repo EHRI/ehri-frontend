cd18ece3a35aa71be2e4b75af3e9d6b86b128a0d
Mike Bryant <mikesname@gmail.com>
2015-08-24 17:28:58 +0100

    Read resources as UTF-8. Prevents encoding issues on Windows.

--- modules/core/test/helpers/ResourceUtils.scala
+++ modules/core/test/helpers/ResourceUtils.scala
@@ -1,12 +1,12 @@
 package helpers
 
-import scala.io.Source
+import scala.io.{Codec, Source}
 
 /**
  * @author Mike Bryant (http://github.com/mikesname)
  */
 trait ResourceUtils {
   protected def resourceAsString(name: String): String = Source.fromInputStream(getClass
-    .getClassLoader.getResourceAsStream(name))
+    .getClassLoader.getResourceAsStream(name))(Codec.UTF8)
     .getLines().mkString("\n")
 }
