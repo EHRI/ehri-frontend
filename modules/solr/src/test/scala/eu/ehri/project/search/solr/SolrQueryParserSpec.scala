ac83b31affef36e1d07d110a6f108c292406e202
Mike Bryant <mikesname@gmail.com>
2015-08-24 09:50:12 +0100

    Fix the code for #233 that broke on edge cases.
    When spelling was bad but Solr couldn't collate the query we hit an
    unfortunate index out of bounds error due to faulty assumptions.
    
    A test has been added for the non-collation case.
    
    Since the XML response parser no longer has feature parity it has
    been removed along with the fixture data.

--- modules/solr/src/test/scala/eu/ehri/project/search/solr/SolrQueryParserSpec.scala
+++ modules/solr/src/test/scala/eu/ehri/project/search/solr/SolrQueryParserSpec.scala
@@ -12,52 +12,7 @@ import models.base.Description
 class SolrQueryParserSpec extends PlaySpecification with ResourceUtils {
 
   implicit val app = new play.api.inject.guice.GuiceApplicationBuilder().build()
-  private def xmlResponseString: String = resourceAsString("solrQueryResponse1.xml")
   private def jsonResponseString: String = resourceAsString("solrQueryResponse1.json")
-
-  "Solr XML Query Parser" should {
-    val xmlHandler = XmlResponseHandler(app)
-    "parse the correct number of docs with the right IDs" in {
-      val qp = xmlHandler.getResponseParser(xmlResponseString)
-      val docs = qp.items
-      docs.size must equalTo(2)
-      docs.find(d => d.itemId == "ehri-cb-638") must beSome
-      docs.find(d => d.itemId == "nl") must beSome
-    }
-
-    "parse facets correctly" in {
-      val qp = xmlHandler.getResponseParser(xmlResponseString)
-      val allFacets = List(
-        FieldFacetClass(
-          key=Description.LANG_CODE,
-          name= "documentaryUnit." + Description.LANG_CODE,
-          param="lang"
-        )
-      )
-      val facetData = qp.extractFacetData(List.empty, allFacets)
-      facetData.size must equalTo(1)
-      facetData.head.count must equalTo(2)
-      facetData.head.facets.head.value must equalTo("eng")
-      facetData.head.facets(1).value must equalTo("fre")
-      facetData.head.facets.headOption must beSome.which { top =>
-        top.value must equalTo("eng")
-      }
-    }
-
-    "parse highlighting correctly" in {
-      val qp = xmlHandler.getResponseParser(xmlResponseString)
-      val highlightMap: Map[String, Map[String, Seq[String]]] = qp.highlightMap
-      highlightMap.size must equalTo(2)
-      val itemMap = highlightMap.get("ehri-cb-638-eng-292")
-      itemMap must beSome
-      val place = itemMap.get.get("place_t")
-      place must beSome
-      place.get must equalTo(Seq("Active in the Netherlands, <em>Amsterdam</em>."))
-
-      val doc1 = qp.items.head
-      doc1.highlights.get("place_t").get must equalTo(Seq("Active in the Netherlands, <em>Amsterdam</em>."))
-    }
-  }
 
   "Solr JSON Query Parser" should {
     val jsonHandler = JsonResponseHandler(app)
@@ -110,6 +65,13 @@ class SolrQueryParserSpec extends PlaySpecification with ResourceUtils {
       val qp = jsonHandler.getResponseParser(jsonResponseString)
       qp.spellcheckSuggestion must beSome.which { case (_, corrected) =>
         corrected must equalTo("purposely mispelled")
+      }
+    }
+
+    "parse non-collated spellcheck data correctly" in {
+      val qp = jsonHandler.getResponseParser(resourceAsString("solrQueryResponse2.json"))
+      qp.spellcheckSuggestion must beSome.which { case (_, corrected) =>
+        corrected must equalTo("purposely")
       }
     }
   }
