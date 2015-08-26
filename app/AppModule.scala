79d2d74ef07c6b9a0e831faa1c72d6cd96e9ecfe
Mike Bryant <mikesname@gmail.com>
2015-08-26 16:47:03 +0100

    Add another search index wrapper that uses the search tools in-process.
    The main advantage here is that deployment and dev setup is easier.
    Otherwise, it should more or less work the same. The downside is yet
    more stuff on the classpath.

--- app/AppModule.scala
+++ app/AppModule.scala
@@ -1,3 +1,5 @@
+import javax.inject.{Inject, Provider}
+
 import auth.AccountManager
 import auth.oauth2.{OAuth2Flow, WebOAuth2Flow}
 import backend._
@@ -8,21 +10,27 @@ import backend.parse.ParseFeedbackDAO
 import backend.rest.cypher.{Cypher, CypherDAO}
 import backend.rest.{RestBackend, CypherIdGenerator, GidSearchResolver, SearchDAO}
 import com.google.inject.AbstractModule
+import eu.ehri.project.indexing.index.Index
+import eu.ehri.project.indexing.index.impl.SolrIndex
 import eu.ehri.project.search.solr.{JsonResponseHandler, ResponseHandler, SolrSearchEngine}
 import global.{AppGlobalConfig, GlobalConfig, GlobalEventHandler}
-import indexing.CmdlineIndexer
+import indexing.SearchToolsIndexer
 import models.{DatabaseGuideDAO, GuideDAO}
 import utils.search.{SearchEngine, SearchIndexer, SearchItemResolver}
 import utils.{DbMovedPageLookup, MovedPageLookup}
 import views.{MarkdownRenderer, PegDownMarkdownRendererProvider}
 
+private class SolrIndexProvider @Inject()(config: play.api.Configuration) extends Provider[Index] {
+  override def get(): Index = new SolrIndex(utils.serviceBaseUrl("solr", config))
+}
 
 class AppModule extends AbstractModule {
   protected def configure(): Unit = {
     bind(classOf[AccountManager]).to(classOf[auth.sql.SqlAccountManager])
     bind(classOf[GlobalConfig]).to(classOf[AppGlobalConfig])
+    bind(classOf[Index]).toProvider(classOf[SolrIndexProvider])
     bind(classOf[ResponseHandler]).to(classOf[JsonResponseHandler])
-    bind(classOf[SearchIndexer]).to(classOf[CmdlineIndexer])
+    bind(classOf[SearchIndexer]).to(classOf[SearchToolsIndexer])
     bind(classOf[SearchEngine]).to(classOf[SolrSearchEngine])
     bind(classOf[SearchItemResolver]).to(classOf[GidSearchResolver])
     bind(classOf[EventHandler]).to(classOf[GlobalEventHandler])
