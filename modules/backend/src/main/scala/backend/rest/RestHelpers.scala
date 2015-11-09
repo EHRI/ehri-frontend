73058a0950da2d06284327104e8e555ba40ec8e4
Mike Bryant <mikesname@gmail.com>
2015-11-09 09:41:50 +0000

    Update Neo4j, backend, and Scala version.
     - Bumped backend to 0.11.1, Neo4j to 2.3.0, and Scala to 2.11.7.
     - Some additional cleanups to build and logger config.
     - Don't chunk Cypher responses when query came via Ajax.
    
    JS doesn't handle chunked responses very nicely. Also tweak some
    Cypher queries to use MATCH and labels instead of START.

--- modules/backend/src/main/scala/backend/rest/RestHelpers.scala
+++ modules/backend/src/main/scala/backend/rest/RestHelpers.scala
@@ -1,7 +1,7 @@
 package backend.rest
 
 import play.api.cache.CacheApi
-import play.api.libs.json.{JsString, JsValue}
+import play.api.libs.json.JsValue
 import defines.EntityType
 import play.api.libs.concurrent.Execution.Implicits._
 import scala.concurrent.Future
@@ -14,24 +14,17 @@ trait RestHelpers {
   implicit def cache: CacheApi
   def cypher: Cypher
 
-  def parseUsers(json: JsValue): Seq[(String, String)] = {
+  private def parseIds(json: JsValue): Seq[(String, String)] = {
     (json \ "data").as[Seq[Seq[String]]].flatMap {
       case x :: y :: _ => Some((x, y))
       case _ => None
     }
   }
 
-  def getGroupList: Future[Seq[(String,String)]] = {
-    cypher.cypher("START n=node:entities(__ISA__ = {isA}) RETURN n.__ID__, n.name",
-        Map("isA" -> JsString(EntityType.Group))).map { goe =>
-      parseUsers(goe)
-    }    
-  }
+  private def getTypeIdAndName(s: EntityType.Value): Future[Seq[(String, String)]] =
+    cypher.cypher(s"MATCH (n:$s) RETURN n.__ID__, n.name").map(parseIds)
+
+  def getGroupList: Future[Seq[(String,String)]] = getTypeIdAndName(EntityType.Group)
   
-  def getUserList: Future[Seq[(String,String)]] = {
-    cypher.cypher("START n=node:entities(__ISA__ = {isA}) RETURN n.__ID__, n.name",
-        Map("isA" -> JsString(EntityType.UserProfile))).map { goe =>
-      parseUsers(goe)
-    }    
-  }  
+  def getUserList: Future[Seq[(String,String)]] = getTypeIdAndName(EntityType.UserProfile)
 }
