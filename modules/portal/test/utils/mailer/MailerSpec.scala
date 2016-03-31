5f5f5af984456a2ba5ae478d5669b1fe0a06ddef
Mike Bryant <mikesname@gmail.com>
2016-03-31 12:02:09 +0100

    Add a test that exercises the mailer.
    This is awkward to get running on CI, and relies on a dummy Python
    SMTP server configured in the .travis.yml, running on port 2500.

---
+++ modules/portal/test/utils/mailer/MailerSpec.scala
@@ -0,0 +1,28 @@
+package utils.mailer
+
+import play.api.Application
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.mailer.{Email, MailerClient}
+import play.api.test.{PlaySpecification, WithApplication}
+
+class MailerSpec extends PlaySpecification {
+
+  val app: Application = new GuiceApplicationBuilder()
+    .configure(
+      "play.mailer.host" -> "localhost",
+      // NB: SMTP_PORT is set in the travis config to 2500
+      "play.mailer.port" -> scala.util.Properties.envOrElse("SMTP_PORT", "25")
+    ).build()
+
+  "SMTP mailer" should {
+    "send mails" in new WithApplication(app) {
+      val mailer = app.injector.instanceOf[MailerClient]
+      val email = new Email(
+        "Test Email",
+        "Test FROM <test@example.com>",
+        Seq("Test TO <test@example.com>"))
+      mailer.send(email)
+      success
+    }
+  }
+}
