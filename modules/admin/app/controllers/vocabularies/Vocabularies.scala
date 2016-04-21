18e0b36ac5b07c656bb241225567d011ae5b6ed2
Mike Bryant <mikesname@gmail.com>
2016-04-21 09:40:20 +0100

    Fix SKOS export from admin and add a test

--- modules/admin/app/controllers/vocabularies/Vocabularies.scala
+++ modules/admin/app/controllers/vocabularies/Vocabularies.scala
@@ -193,7 +193,7 @@ case class Vocabularies @Inject()(
 
   def exportSkos(id: String, format: Option[String]) = OptionalUserAction.async { implicit request =>
     val baseUrl: Option[String] = request.getQueryString("baseUri")
-    ws.url(utils.serviceBaseUrl("ehridata", config) + s"/${EntityType.Vocabulary}/$id/export")
+    ws.url(utils.serviceBaseUrl("ehridata", config) + s"/classes/${EntityType.Vocabulary}/$id/export")
         .withQueryString(format.toSeq.map(f => "format" -> f): _*)
         .withQueryString(baseUrl.toSeq.map(url => "baseUri" -> url): _*)
         .withHeaders(request.userOpt.map(u => Constants.AUTH_HEADER_NAME -> u.id).toSeq: _*).get().map { r =>
