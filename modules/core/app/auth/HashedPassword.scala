ca59321e64aa2e20c479c85d76bd26ff9cfa8a9d
Mike Bryant <mikesname@gmail.com>
2016-07-29 15:22:26 +0100

    Minor cleanups in account manager

--- modules/core/app/auth/HashedPassword.scala
+++ modules/core/app/auth/HashedPassword.scala
@@ -9,34 +9,6 @@ import org.mindrot.jbcrypt.BCrypt
 object HashedPassword {
   def fromPlain(plain: String) = new HashedPassword(BCrypt.hashpw(plain.toString, BCrypt.gensalt()))
   def fromHashed(hashed: String) = new HashedPassword(hashed)
-
-  import anorm.{TypeDoesNotMatch, Column, ToStatement, ParameterMetaData}
-
-  /**
-   * Implicit conversion from HashedPassword to Anorm statement value
-   */
-  implicit def pwToStatement: ToStatement[HashedPassword] = new ToStatement[HashedPassword] {
-    def set(s: java.sql.PreparedStatement, index: Int, aValue: HashedPassword): Unit =
-      s.setString(index, aValue.s)
-  }
-
-  implicit object HashedPasswordParameterMetaData extends ParameterMetaData[HashedPassword] {
-    val sqlType = ParameterMetaData.StringParameterMetaData.sqlType
-    val jdbcType = ParameterMetaData.StringParameterMetaData.jdbcType
-  }
-
-  /**
-   * Implicit conversion from Anorm row to HashedPassword
-   */
-  implicit def rowToPw: Column[HashedPassword] = {
-    Column.nonNull1[HashedPassword] { (value, meta) =>
-      value match {
-        case v: String => Right(HashedPassword.fromHashed(v))
-        case _ => Left(TypeDoesNotMatch(
-          s"Cannot convert $value:${value.asInstanceOf[AnyRef].getClass} to hashed password for column ${meta.column}"))
-      }
-    }
-  }
 }
 
 case class HashedPassword private(s: String) {
