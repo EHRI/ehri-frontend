8970a3351f69841c9d6c692cca5625c67042318c
Mike Bryant <mikesname@gmail.com>
2016-11-29 14:58:02 +0000

    S3 upload using Alpakka (not yet working)

--- modules/portal/app/services/storage/S3FileStorage.scala
+++ modules/portal/app/services/storage/S3FileStorage.scala
@@ -4,18 +4,33 @@ import java.io.File
 import java.net.URI
 import javax.inject.Inject
 
-import awscala._
-import awscala.s3._
+import akka.actor.ActorSystem
+import akka.stream.Materializer
+import akka.stream.alpakka.s3.auth.AWSCredentials
+import akka.stream.alpakka.s3.scaladsl.{MultipartUploadResult, S3Client}
+import akka.stream.scaladsl.{FileIO, Sink}
+import akka.util.ByteString
+import play.api.Logger
 
 import scala.concurrent.{ExecutionContext, Future}
 
-case class S3FileStorage @Inject ()(config: play.api.Configuration) extends FileStorage {
+case class S3FileStorage @Inject ()(implicit config: play.api.Configuration,
+    actorSystem: ActorSystem, materializer: Materializer) extends FileStorage {
+
+  private val logger = Logger(getClass)
+
   override def putFile(instance: String, classifier: String, path: String, file: File)(
       implicit executionContext: ExecutionContext): Future[URI] = {
+
     val s3config: AwsConfig = AwsConfig.fromConfig(config, fallback = Map("aws.instance" -> instance))
-    implicit val s3 = S3(Credentials(s3config.accessKey, s3config.secret))(awscala.Region(s3config.region))
-    val bucket: Bucket = s3.bucket(classifier).getOrElse(sys.error(s"Bucket $classifier not found"))
-    val read: PutObjectResult = bucket.putAsPublicRead(path, file)
-    Future.successful(new URI(s"https://${read.bucket.name}.s3-${s3config.region}.amazonaws.com/${read.key}"))
+    val cred = AWSCredentials(s3config.accessKey, s3config.secret)
+
+    val client = new S3Client(cred, s3config.region)
+    println(s"Uploading file: ${file.getPath} to $classifier:$path")
+
+    val sink: Sink[ByteString, Future[MultipartUploadResult]] = client.multipartUpload(classifier, path, chunkingParallelism = 1)
+    
+
+    FileIO.fromPath(file.toPath).runWith(sink).map(r => new URI(r.location.toString))
   }
 }
