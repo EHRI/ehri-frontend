3a6cc0e6be826564060ff9a68ebf3fbc6caf4b07
Mike Bryant <mikesname@gmail.com>
2017-04-20 17:05:18 +0100

    Remove flash message on OpenID failure, since it's not a redirect.

--- modules/core/app/controllers/core/auth/openid/OpenIDLoginHandler.scala
+++ modules/core/app/controllers/core/auth/openid/OpenIDLoginHandler.scala
@@ -113,7 +113,6 @@ trait OpenIDLoginHandler extends AccountHelpers {
       } recoverWith {
         case t => block(OpenIdCallbackRequest(
           Left(openidForm.withGlobalError("error.openId", t.getMessage)), request))
-          .map(_.flashing("danger" -> Messages("error.openId", t.getMessage)))
       }
     }
   }
