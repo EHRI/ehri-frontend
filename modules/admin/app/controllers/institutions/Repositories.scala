3e9f7f52cc9c1b9c58ecfe11e4fc71b46477186f
Mike Bryant <mikesname@gmail.com>
2017-08-09 15:24:46 +0100

    UI, and attempts (unsuccessful so far) to get a reliable event stream incorporating relocation and reindexing.

--- modules/admin/app/controllers/institutions/Repositories.scala
+++ modules/admin/app/controllers/institutions/Repositories.scala
@@ -250,7 +250,7 @@ case class Repositories @Inject()(
   }
 
   def ingest(id: String): Action[AnyContent] = (AdminAction andThen ItemPermissionAction(id)).apply { implicit request =>
-    Ok(views.html.admin.utils.ingest(request.item, IngestParams.ingestForm,
-      controllers.admin.routes.Utils.ingestPost(id, "ead-sync"), sync = true))
+    Ok(views.html.admin.utils.ingest(request.item, None, IngestParams.ingestForm,
+      controllers.admin.routes.Utils.ingestPost(request.item.isA, id, "ead-sync"), sync = true))
   }
 }
