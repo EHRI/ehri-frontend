7044f06ccc74a5d95550b90f821522b218518313
Mike Bryant <mikesname@gmail.com>
2017-08-25 11:18:02 +0100

    Rework the feedback dropdown so it uses Bootstrap JS (mostly).
    Also fixes feedback form in responsive view.

--- modules/portal/app/assets/js/feedback.js
+++ modules/portal/app/assets/js/feedback.js
@@ -1,94 +1,42 @@
 // Handle slide-out feedback form
 //
 
-jQuery(function($) {
+jQuery(function ($) {
   // handle feedback form submission...
-  var $handle = $(".feedback-handle"),
-      container = $(".feedback"),
-      $cancel = $(".feedback-cancel", container),
-      $form = $(".feedback-form", container),
-      $submit = $(".feedback-submit", container),
-      $close = $(".feedback-close", container),
-      $thanks = $(".feedback-thanks", container),
+  var $container = $(".feedback-container"),
+      $toggle = $("#feedback-toggle", $container),
+      $form = $(".feedback-form", $container),
+      $submit = $(".feedback-submit", $container),
+      $thanks = $(".feedback-thanks", $container),
       $email = $("input[name='email']", $form),
       $text = $("textarea[name='text']", $form);
 
-  // Trigger closing of the feedback form when clicking
-  // outside its container, or on or inside the close
-  // trigger.
-  var feedbackOut = function(e) {
-    if($(e.target).parents(".feedback-container").length != 1
-        || $(e.target).hasClass("feedback-close")
-        || $(e.target).parent().hasClass("feedback-close")) {
-      $handle.trigger("click");
-    }
-  };
-
-  $handle.on("click", function(e) {
-    e.preventDefault();
-    // Add a close button when the form becomes
-    // visible. Remove it on hiding.
-    container.toggle(300, function() {
-      if(container.is(":visible")) {
-        $form.append(
-          $("<a />", {
-            "class" : "feedback-close",
-            "html" : "<span class='glyphicon glyphicon-remove'></span>"
-          })
-        );
-        $(document).bind("click", feedbackOut);
-      } else {
-        $(document).unbind("click", feedbackOut);
-        container.find(".feedback-close").remove();
-      }
-    });
-  });
-
   $form.validate({
-    showErrors: function(em, el) {}
+    showErrors: function (em, el) {
+    }
   });
 
   $submit.prop("disabled", true);
 
-  $email.on("blur", function(event) {
+  $email.on("blur", function (event) {
     $email.parent(".form-group")
         .toggleClass("has-error", !$email.valid());
   });
 
-  $form.on("keyup", function(event) {
+  $form.on("keyup", function (event) {
     $submit.prop("disabled", !$form.valid());
   });
 
-  $close.on("click", function() {
-    $handle.trigger("click");
-  });
-
-  $cancel.on("click", function(e) {
-    e.preventDefault();
-    $text.val("");
-    $handle.trigger("click");
-  });
-
-  $submit.on("click", function(event) {
+  $form.submit(function (event) {
     event.preventDefault();
     $submit.prop("disabled", true);
-    $.post($form.attr("action"), $form.serialize(), function(data, textStatus) {
-
-      // Hide the form and show the thanks message...
-      $form.hide(100, function() {
-        $thanks.slideDown(500, function() {
-          setTimeout(function() {
-            $text.val("");
-
-            $thanks.slideUp(500, function() {
-              $handle.trigger("click").queue(function(next) {
-                container.find("form").show();
-                next();
-              });
-
-            });
-          }, 1000);
-        });
+    $.post($form.attr("action"), $form.serialize(), function (data) {
+      $thanks.slideDown(500, function () {
+        setTimeout(function () {
+          $toggle.dropdown("toggle");
+          $text.val("");
+          $thanks.hide();
+        }, 1000);
       });
     });
   });
