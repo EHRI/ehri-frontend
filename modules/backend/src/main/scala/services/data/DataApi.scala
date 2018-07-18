c20914f63086f7a4e5853b02f471e770f31009dc
Mike Bryant <mikesname@gmail.com>
2018-07-18 16:29:35 +0200

    Delete now-unneeded API calls for description CRUD

--- modules/backend/src/main/scala/services/data/DataApi.scala
+++ modules/backend/src/main/scala/services/data/DataApi.scala
@@ -369,30 +369,6 @@ trait DataApiHandle {
   def history[A: Readable](id: String, params: RangeParams, filters: SystemEventParams = SystemEventParams.empty): Future[RangePage[Seq[A]]]
 
   /**
-    * Create a new description on a given item.
-    *
-    * @param id     the item's ID
-    * @param desc   the description data
-    * @param logMsg an optional log message
-    * @tparam MT the item's meta type
-    * @tparam DT the item's description type
-    */
-  def createDescription[MT: Resource, DT: Writable](id: String, desc: DT, logMsg: Option[String] = None): Future[DT]
-
-  /**
-    * Update a description on a given item.
-    *
-    * @param id     the item's ID
-    * @param did    the description ID
-    * @param desc   the description data
-    * @param logMsg an optional log message
-    * @tparam MT the item's meta type
-    * @tparam DT the item's description type
-    * @return
-    */
-  def updateDescription[MT: Resource, DT: Writable](id: String, did: String, desc: DT, logMsg: Option[String] = None): Future[DT]
-
-  /**
     * Create a new access point on the given item description.
     *
     * @param id     the item's ID
@@ -403,16 +379,6 @@ trait DataApiHandle {
     * @tparam AP the access point type
     */
   def createAccessPoint[MT: Resource, AP: Writable](id: String, did: String, ap: AP, logMsg: Option[String] = None): Future[AP]
-
-  /**
-    * Delete a given description.
-    *
-    * @param id     the item's ID
-    * @param did    the description ID
-    * @param logMsg an optional log message
-    * @tparam MT the access point type
-    */
-  def deleteDescription[MT: Resource](id: String, did: String, logMsg: Option[String] = None): Future[Unit]
 
   /**
     * Delete a given access point.
