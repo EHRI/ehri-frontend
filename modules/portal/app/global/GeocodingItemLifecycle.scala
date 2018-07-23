b7dec5046f6684943e53536bedcf6ffb951e33ee
Mike Bryant <mikesname@gmail.com>
2018-07-23 13:33:24 +0200

    Don't consider geocoding successful unless confidence is high

--- modules/portal/app/global/GeocodingItemLifecycle.scala
+++ modules/portal/app/global/GeocodingItemLifecycle.scala
@@ -16,11 +16,11 @@ case class GeocodingItemLifecycle @Inject()(geocoder: GeocodingService) extends 
   private def address(repo: RepositoryF): Option[AddressF] =
     repo.descriptions.flatMap(_.addresses.find(_.streetAddress.isDefined)).headOption
 
-  private def geocode(repoData: RepositoryF)(implicit ec: ExecutionContext): Future[RepositoryF] = {
+  private def geocode(id: Option[String], repoData: RepositoryF)(implicit ec: ExecutionContext): Future[RepositoryF] = {
     address(repoData) match {
       case Some(a) =>
         geocoder.geocode(a).map { point =>
-          logger.info(s"Geocoding $a (${repoData.id}): $point")
+          logger.info(s"Geocoding $a (${id.getOrElse("???")}): ${point.map(_.toString).getOrElse("???")}")
           repoData.copy(latitude = point.map(_.latitude), longitude = point.map(_.longitude))
         }
       case None => Future.successful(repoData.copy(latitude = None, longitude = None))
@@ -28,14 +28,9 @@ case class GeocodingItemLifecycle @Inject()(geocoder: GeocodingService) extends 
   }
 
   override def preSave[MT <: Model](id: Option[String], item: Option[MT], data: MT#T, event: EventType.Value)(implicit ec: ExecutionContext): Future[MT#T] = {
-    (item, data) match {
-      // The item has been created...
-      case (None, repoData: RepositoryF) =>
-        geocode(repoData).map(_.asInstanceOf[MT#T])
-
-      // The item has been updated and the address has changed...
-      case (Some(r: Repository), repoData: RepositoryF) if address(r.data) != address(repoData) =>
-        geocode(repoData).map(_.asInstanceOf[MT#T])
+    data match {
+      // A repository has been created or updated...
+      case repoData: RepositoryF => geocode(id, repoData).map(_.asInstanceOf[MT#T])
 
       // Not a repository...
       case _ => Future.successful(data)
--- modules/portal/app/services/geocoding/BingGeocodingService.scala
+++ modules/portal/app/services/geocoding/BingGeocodingService.scala
@@ -3,6 +3,7 @@ package services.geocoding
 import javax.inject.Inject
 import models.AddressF
 import play.api.Configuration
+import play.api.libs.json.{JsError, JsSuccess, Json}
 import play.api.libs.ws.WSClient
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -13,37 +14,50 @@ case class BingGeocodingService @Inject()(ws: WSClient, config: Configuration)(i
   private val logger = play.api.Logger(classOf[BingGeocodingService])
   private val bingKey = config.getOptional[String]("bing.key")
 
-  import play.api.libs.json._
-  import play.api.libs.functional.syntax._
-  private implicit def reads: Reads[Point] = (
-    (__ \ "resourceSets" \ 0 \ "resources" \ 0 \ "point" \ "coordinates" \ 0).read[BigDecimal] and
-    (__ \ "resourceSets" \ 0 \ "resources" \ 0 \ "point" \ "coordinates" \ 1).read[BigDecimal]
-  )(Point.apply _)
+  case class Response(point: Point, confidence: String)
+  object Response {
+    import play.api.libs.json._
+    import play.api.libs.functional.syntax._
 
+    implicit def pointReads: Reads[Point] = (
+      (__ \ "point" \ "coordinates" \ 0).read[BigDecimal] and
+      (__ \ "point" \ "coordinates" \ 1).read[BigDecimal]
+    )(Point.apply _)
+
+    implicit def reads: Reads[Response] = (
+      (__ \ "resourceSets" \ 0 \ "resources" \ 0).read[Point] and
+      (__ \ "resourceSets" \ 0 \ "resources" \ 0 \ "confidence").read[String]
+    )(Response.apply _)
+  }
 
   override def geocode(address: AddressF): Future[Option[Point]] = {
 
     val params = Seq(
       "countryRegion" -> address.countryCode,
+      "userRegion" -> address.countryCode,
       "adminDistrict" -> address.region,
       "postalCode" -> address.postalCode,
       "addressLine" -> address.streetAddress,
       "locality" -> address.city,
       "maxResults" -> Some("2"),
-      "key" -> bingKey,
       "includeNeighbourhood" -> Some("false")
     ).collect { case (k, v) => k -> v.getOrElse("-") }
 
-    logger.debug(s"Geocoding address: $address")
+    logger.debug(s"Geocoding address: $params")
     ws.url("https://dev.virtualearth.net/REST/v1/Locations")
-        .withQueryStringParameters(params: _*).get().map { r =>
+        .withQueryStringParameters(params: _*)
+        .addQueryStringParameters("key" -> bingKey.getOrElse("-"))
+        .get().map { r =>
       logger.debug(s"Bing response: ${Json.prettyPrint(r.json)}")
-      r.json.validate[Point] match {
-        case JsSuccess(point, _) =>
+      r.json.validate[Response] match {
+        case JsSuccess(Response(point, "High"), _) =>
           logger.debug(s"Geocode for $address: $point")
           Some(point)
+        case JsSuccess(Response(point, confidence), _) =>
+          logger.warn(s"Geocode confidence ($confidence) not high for: $address")
+          None
         case JsError(e) =>
-          logger.warn(s"Unable to parse Geocode response: $e")
+          logger.error(s"Unable to parse Geocode response: $e")
           None
       }
     } recover {
