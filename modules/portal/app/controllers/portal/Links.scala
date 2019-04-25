1b7a8b8d1381f04688cf776968b5a32deea4515a
Mike Bryant <mikesname@gmail.com>
2019-04-25 16:46:08 +0200

    Quick demo leaflet map showing copy-links between repositories.
    This isn't final UI (hence not linked from anywhere) but serves as
    a map example to hopefully spur further development.

--- modules/portal/app/controllers/portal/Links.scala
+++ modules/portal/app/controllers/portal/Links.scala
@@ -1,13 +1,12 @@
 package controllers.portal
 
-import javax.inject.{Inject, Singleton}
-
-import services.cypher.CypherService
 import controllers.AppComponents
 import controllers.generic.Search
 import controllers.portal.base.{Generic, PortalController}
+import javax.inject.{Inject, Singleton}
 import models.Link
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import services.cypher.CypherService
 
 
 @Singleton
@@ -21,5 +20,26 @@ case class Links @Inject()(
 
   def browse(id: String): Action[AnyContent] = GetItemAction(id).apply { implicit request =>
     Ok(views.html.link.show(request.item))
+  }
+
+  def copies: Action[AnyContent] = OptionalUserAction.apply { implicit request =>
+    if (isAjax) Ok.chunked(cypher.legacy(scriptBody =
+        """
+          |MATCH (d1:DocumentaryUnit)<-[:hasLinkSource]-(link:Link)-[:hasLinkTarget]->
+          |       (r2:Repository)<-[:describes]-(rd2:RepositoryDescription),
+          |	  (d1)-[:heldBy|childOf*]->(r1:Repository)<-[:describes]-(rd1:RepositoryDescription)
+          |WHERE r1 <> r2 AND exists(r1.latitude) AND exists(r2.latitude)
+          |RETURN DISTINCT
+          |	r1.__id,
+          | trim(rd1.name),
+          | r1.latitude,
+          | r1.longitude,
+          | r2.__id,
+          | trim(rd2.name),
+          | r2.latitude,
+          | r2.longitude
+          |ORDER BY r1.__id;
+        """.stripMargin)
+    ) else Ok(views.html.link.copies(controllers.portal.routes.Links.copies()))
   }
 }
