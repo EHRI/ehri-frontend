b78a55879a27ca06fcccdea0a6a1f3158ad30fc8
Mike Bryant <mikesname@gmail.com>
2019-04-30 18:19:34 +0200

    Update to Alpakka 1.0.0

--- modules/portal/app/services/storage/S3FileStorage.scala
+++ modules/portal/app/services/storage/S3FileStorage.scala
@@ -7,9 +7,8 @@ import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model._
 import akka.stream.Materializer
-import akka.stream.alpakka.s3.S3Settings
-import akka.stream.alpakka.s3.acl.CannedAcl
-import akka.stream.alpakka.s3.scaladsl.S3Client
+import akka.stream.alpakka.s3.headers.CannedAcl
+import akka.stream.alpakka.s3.scaladsl.S3
 import akka.stream.scaladsl.{FileIO, Source}
 import akka.util.ByteString
 import javax.inject.Inject
@@ -23,15 +22,13 @@ case class S3FileStorage @Inject()(config: play.api.Configuration)(implicit acto
   private val logger = Logger(getClass)
   private implicit val ec: ExecutionContext = mat.executionContext
 
-  private def client = new S3Client(S3Settings(config.underlying))
-
-  override def putBytes(classifier: String, path: String, src: Source[ByteString, _], public: Boolean = false): Future[URI] = {
+  override def putBytes(bucket: String, path: String, src: Source[ByteString, _], public: Boolean = false): Future[URI] = {
     val mediaType: MediaType = MediaTypes.forExtension(path.substring(path.lastIndexOf(".") + 1))
     // FIXME: If the file is binary this is okay, but otherwise it'll only upload with UTF-8 encoding...
     val contentType = ContentType(mediaType, () => HttpCharsets.`UTF-8`)
-    logger.debug(s"Uploading file: $path to $classifier with content-type: $contentType")
+    logger.debug(s"Uploading file: $path to $bucket with content-type: $contentType")
     val acl = if (public) CannedAcl.PublicRead else CannedAcl.AuthenticatedRead
-    val sink = client.multipartUpload(classifier, path, contentType = contentType, cannedAcl = acl)
+    val sink = S3.multipartUpload(bucket, path, contentType = contentType, cannedAcl = acl)
     src.runWith(sink).map(r => new URI(r.location.toString))
   }
 
@@ -39,5 +36,5 @@ case class S3FileStorage @Inject()(config: play.api.Configuration)(implicit acto
     putBytes(classifier, path, FileIO.fromPath(file.toPath), public)
 
   override def listFiles(classifier: String, prefix: Option[String]): Source[String, NotUsed] =
-    client.listBucket(classifier, prefix).map(_.key)
+    S3.listBucket(classifier, prefix).map(_.key)
 }
