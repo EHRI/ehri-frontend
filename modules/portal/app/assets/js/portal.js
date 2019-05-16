3db196e882f9ac4aae3597faa5055deb0cde51a7
Mike Bryant <mikesname@gmail.com>
2019-05-16 16:54:47 +0200

    Use native CSS position: sticky for sidebar affix tricks

--- modules/portal/app/assets/js/portal.js
+++ modules/portal/app/assets/js/portal.js
@@ -44,63 +44,6 @@ jQuery(function ($) {
   // don't support this natively
   $('input, textarea').placeholder();
 
-  function isSafari() {
-    return navigator.userAgent.indexOf("Chrome") === -1 &&
-        navigator.userAgent.indexOf("Safari") !== -1;
-  }
-
-  // Affix side-scrolling sidebars. This is really
-  // dodgy and difficult, and has lots of bugs.
-  // Notably, if the sidebar is the deepest element in
-  // it's container then the page tends to jump up and
-  // down on scroll. To try and prevent this we only affix
-  // the sidebar TOC if there's some minumum padding between
-  // the bottom of the sidebar and the bottom of the container,
-  // suggesting other content is enlarging the container.
-  // NB: Note that this system depends on multiple descriptions
-  // being collapsed, though that should be the case since the
-  // JS is loaded before the page, not after it. Still, I'd be
-  // very surprised if there were not more gremlins here.
-  //
-  // Additionally, due to bug http://github.com/twbs/bootstrap/issues/12126
-  // we can't use affix in Safari
-  $(".sidepanel-toc").each(function () {
-    var $target = $(this),
-        $prev = $target.prev(),
-        $parent = $target.closest(".item-details"),
-        $minPad = 100;
-
-    var $parentHeight = $parent.offset().top + $parent.outerHeight(true),
-        $targetHeight = $target.offset().top + $target.outerHeight(true);
-
-    if ($parentHeight > $targetHeight + $minPad && !isSafari()) {
-      // FIXME: BS4
-      // $target.affix({
-      //   offset: {
-      //     top: function () {
-      //       return (this.top = $prev.offset().top + $prev.outerHeight(true));
-      //     },
-      //     bottom: function () {
-      //       // the distance of the bottom of the target from the bottom
-      //       // of the document. In this case we want
-      //       var bodyHeight = $(document).outerHeight(true);
-      //       var parentPos = $parent.position().top + $parent.outerHeight(true);
-      //       return (this.bottom = bodyHeight - parentPos);
-      //     }
-      //   }
-      // });
-    }
-  });
-
-  // Hack to fix affix on pressing home button:
-  // https://github.com/twbs/bootstrap/issues/9609#issuecomment-22840954
-  $(window).on('keyup', function (e) {
-    // key code taken from http://www.quirksmode.org/js/keys.html
-    // safari fires 63273 instead of 36
-    (e.keyCode === 36 || e.keyCode === 63273) && $(window).trigger('scroll')
-  });
-
-
 // Facet label tooltips
   $(".facet-label").tooltip({
     placement: "top",
