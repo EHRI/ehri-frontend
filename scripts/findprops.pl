802bbfba33f8836bb3cbef03bf6fe17fd58f078b
Mike Bryant <mikesname@gmail.com>
2019-06-06 09:05:29 +0200

    Make messages update script more flexible, with a CSV option

--- scripts/findprops.pl
+++ scripts/findprops.pl
@@ -5,34 +5,54 @@
 # e.g. untranslated strings.
 #
 
+use v5.10;
+use utf8;
+
 use strict;
-use warnings;
+use warnings qw(FATAL utf8);
 use Config::Properties;
+use Text::CSV qw(csv);
+use Getopt::Long qw(GetOptions);
+use open qw(:utf8 :std);
+
+my $is_csv = 0;
+
+GetOptions("c|csv" => \$is_csv) or die ("Error in command line arguments");
 
 if (scalar(@ARGV) != 2) {
     die "usage: findprops.pl <allprops> <partial>\n";
 }
 
-compare_props(parse_props(shift(@ARGV)), parse_props(shift(@ARGV)));
+my @data = compare_props(parse_props(shift(@ARGV)), parse_props(shift(@ARGV)));
+
+if ($is_csv) {
+    my $csv = Text::CSV->new ({ binary => 0, auto_diag => 1 });
+    $csv->say(*STDOUT, $_) for @data;
+} else {
+    my $conf = Config::Properties->new();
+    $conf->setProperty(@{$_}) for @data;
+    $conf->store(\*STDOUT);
+}
 
 exit(0);
+
 
 sub compare_props {
     my ($all, $partial) = @_;
 
-    my $data = Config::Properties->new();
+    my @data = ();
 
     for my $key ($all->propertyNames) {
         if (not defined $partial->getProperty($key)) {
             my $val = $all->getProperty($key);
-            if ($val ne "") {
-                $data->setProperty($key, $val);
+            $val =~ s/\s*\\\s*\R\s+/ /mg;
+            if ($val) {
+                push @data, [$key, $val];  
             }
         }
     }
 
-    open(my $OUT_H, '>&', \*STDOUT) or die;
-    $data->store($OUT_H);
+    return @data;
 }
 
 sub parse_props {
