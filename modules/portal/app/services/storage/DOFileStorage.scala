169cb5758f6133297c54a5508ccf4a10b712f64e
Mike Bryant <mikesname@gmail.com>
2020-02-04 17:10:39 +0100

    Prototype a rough way to upload file data for institutions.
    This could, one day, perhaps be ingest data. At the moment it is
    mainly just a test of using multiple S3-compatible Cloud APIs.
    Ideally uploaded data would go directly to the Cloud rather than
    via our server but more work is needed for facility that, if it's
    even possible with DO. Note that DO does not seem to support versioning
    yet, which is a bit of a showstopper.

--- modules/portal/app/services/storage/DOFileStorage.scala
+++ modules/portal/app/services/storage/DOFileStorage.scala
@@ -1,25 +1,45 @@
 package services.storage
 
+import java.io.File
 import java.net.URI
 
 import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model._
 import akka.stream.Materializer
+import akka.stream.alpakka.s3.{S3Attributes, S3Ext}
 import akka.stream.alpakka.s3.headers.CannedAcl
 import akka.stream.alpakka.s3.scaladsl.S3
 import akka.stream.scaladsl.{FileIO, Source}
 import akka.util.ByteString
+import com.amazonaws.auth.{AWSCredentials, AWSCredentialsProvider}
+import com.amazonaws.regions.AwsRegionProvider
 import javax.inject.Inject
 import play.api.Logger
 
 import scala.concurrent.{ExecutionContext, Future}
 
 
-case class S3FileStorage @Inject()(config: play.api.Configuration)(implicit actorSystem: ActorSystem, mat: Materializer) extends FileStorage {
+case class DOFileStorage @Inject()(config: play.api.Configuration)(implicit actorSystem: ActorSystem, mat: Materializer) extends FileStorage {
 
   private val logger = Logger(getClass)
   private implicit val ec: ExecutionContext = mat.executionContext
+  private val creds = new AWSCredentialsProvider {
+    override def getCredentials: AWSCredentials = new AWSCredentials {
+      override def getAWSAccessKeyId: String = config.get[String]("digitalocean.spaces.access-key-id")
+      override def getAWSSecretKey: String = config.get[String]("digitalocean.spaces.secret-access-key")
+    }
+    override def refresh(): Unit = {}
+  }
+  private val region = new AwsRegionProvider {
+    override def getRegion: String = config.get[String]("digitalocean.spaces.region")
+  }
+  private val doEndpoint = S3Ext(actorSystem)
+    .settings
+    .withEndpointUrl(config.get[String]("digitalocean.spaces.endpoint"))
+    .withCredentialsProvider(creds)
+    .withS3RegionProvider(region)
+
 
   override def putBytes(bucket: String, path: String, src: Source[ByteString, _], public: Boolean = false): Future[URI] = {
     val mediaType: MediaType = MediaTypes.forExtension(path.substring(path.lastIndexOf(".") + 1))
@@ -27,7 +47,10 @@ case class S3FileStorage @Inject()(config: play.api.Configuration)(implicit acto
     val contentType = ContentType(mediaType, () => HttpCharsets.`UTF-8`)
     logger.debug(s"Uploading file: $path to $bucket with content-type: $contentType")
     val acl = if (public) CannedAcl.PublicRead else CannedAcl.AuthenticatedRead
+
     val sink = S3.multipartUpload(bucket, path, contentType = contentType, cannedAcl = acl)
+        .withAttributes(S3Attributes.settings(doEndpoint))
+
     src.runWith(sink).map(r => new URI(r.location.toString))
   }
 
@@ -36,4 +59,5 @@ case class S3FileStorage @Inject()(config: play.api.Configuration)(implicit acto
 
   override def listFiles(classifier: String, prefix: Option[String]): Source[File, NotUsed] =
     S3.listBucket(classifier, prefix).map(f => File(f.key, f.lastModified, f.size))
+      .withAttributes(S3Attributes.settings(doEndpoint))
 }
