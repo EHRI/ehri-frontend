e43ee82e86c1d2978d5af4155a978f1e564affe5
Mike Bryant <mikesname@gmail.com>
2020-03-18 11:33:21 +0000

    Refactoring S3 compatible storage to use a common delegate.
    This still needs more work.

---
+++ modules/portal/app/services/storage/FileMeta.scala
@@ -0,0 +1,22 @@
+package services.storage
+
+import java.time.Instant
+
+import play.api.libs.json.{Json, Writes}
+
+/**
+  * A representation of a stored file.
+  *
+  * @param key         the unique key of this file
+  * @param lastModifed the last modified time
+  * @param size        the size, in bytes
+  */
+case class FileMeta(
+  key: String,
+  lastModifed: Instant,
+  size: Long
+)
+
+object FileMeta {
+  implicit val _toJson: Writes[FileMeta] = Json.writes[FileMeta]
+}
