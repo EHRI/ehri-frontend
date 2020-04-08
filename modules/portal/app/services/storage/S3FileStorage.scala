36966cdc23dd8f2d61aa405849224227d57b6ba0
Mike Bryant <mikesname@gmail.com>
2020-04-08 18:21:41 +0100

    Expand the file storage API to facilitate easier testing w/o network

--- modules/portal/app/services/storage/S3FileStorage.scala
+++ modules/portal/app/services/storage/S3FileStorage.scala
@@ -33,6 +33,9 @@ case class S3FileStorage @Inject()(config: play.api.Configuration)(implicit acto
 
   private val ops = S3CompatibleOperations(None, creds, region)
 
+  override def get(classifier: String, path: String): Future[Option[(FileMeta, Source[ByteString, _])]] =
+    ops.get(classifier, path)
+
   override def uri(classifier: String, path: String, duration: FiniteDuration = 10.minutes, contentType: Option[String] = None): URI =
     ops.uri(classifier, path, duration, contentType)
 
