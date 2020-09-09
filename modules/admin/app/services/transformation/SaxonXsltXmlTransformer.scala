57149f448f50e28fc790e25b7041a446f2dd04b1
Mike Bryant <mikesname@gmail.com>
2020-09-09 15:57:53 +0100

    Use the native Saxon XSLT API rather than Java's generic one
    This prevents different environments picking different XSLT proc
    implementations, which can be unpredictable.

--- modules/admin/app/services/transformation/SaxonXsltXmlTransformer.scala
+++ modules/admin/app/services/transformation/SaxonXsltXmlTransformer.scala
@@ -2,8 +2,9 @@ package services.transformation
 
 import java.io.{StringReader, StringWriter}
 
-import javax.xml.transform.stream.{StreamResult, StreamSource}
-import javax.xml.transform.{TransformerConfigurationException, TransformerFactory}
+import javax.xml.transform.TransformerConfigurationException
+import javax.xml.transform.stream.StreamSource
+import net.sf.saxon.s9api.{Processor, SaxonApiException, Serializer}
 import org.xml.sax.SAXParseException
 
 
@@ -12,18 +13,23 @@ case class SaxonXsltXmlTransformer() extends XsltXmlTransformer {
   def transform(input: String, mapping: String): String = {
 
     val writer: StringWriter = new StringWriter()
-    val resultWriter = new StreamResult(writer)
-
     val mapSource: StreamSource = new StreamSource(new StringReader(mapping))
     val inputSource: StreamSource = new StreamSource(new StringReader(input))
 
-    try {
-      val factory = TransformerFactory.newInstance()
-      val transformer = factory.newTransformer(mapSource)
-      transformer.transform(inputSource, resultWriter)
+    val processor = new Processor(false)
+    val compiler = processor.newXsltCompiler()
 
+    try {
+      val stylesheet = compiler.compile(mapSource)
+      val out = processor.newSerializer(writer)
+      out.setOutputProperty(Serializer.Property.OMIT_XML_DECLARATION, "yes")
+      out.setOutputProperty(Serializer.Property.INDENT, "yes")
+
+      val transformer = stylesheet.load30()
+      transformer.transform(inputSource, out)
       writer.toString
     } catch {
+      case e: SaxonApiException => throw InvalidMappingError(e.getLocalizedMessage)
       case e: TransformerConfigurationException => throw InvalidMappingError(e.getLocalizedMessage)
       case e: SAXParseException => throw InvalidMappingError(e.getLocalizedMessage)
     }
