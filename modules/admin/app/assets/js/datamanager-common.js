d7da901a8386144a6c687c75b265f30562c82cb0
Mike Bryant <mikesname@gmail.com>
2020-09-11 14:39:04 +0100

    Add a modal confirmation when deleting transformations.

--- modules/admin/app/assets/js/datamanager-common.js
+++ modules/admin/app/assets/js/datamanager-common.js
@@ -338,6 +338,44 @@ let previewPanelMixin = {
   `
 };
 
+Vue.component("modal-alert", {
+  props: {
+    title: String,
+    cls: {
+      type: String,
+      default: 'danger'
+    },
+    accept: {
+      type: String,
+      default: "Okay"
+    }
+  },
+  mounted() {
+    this.$el.querySelector("button[data-dismiss='modal']").focus();
+  },
+  template: `
+    <div v-bind:class="cls" class="modal modal-alert" tabindex="-1" role="dialog">
+      <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">{{title}}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close" v-on:click="$emit('close')" tabindex="-1">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <slot></slot>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn" data-dismiss="modal" v-on:click="$emit('close')" autofocus>Cancel</button>
+            <button type="button" class="btn" v-bind:class="'btn-' + cls" v-on:click="$emit('accept')">{{accept}}</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `
+});
+
 Vue.component("file-picker-suggestion", {
   props: {selected: Boolean, item: Object,},
   template: `
