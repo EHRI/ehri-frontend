878291bdad11480b962de3ded85df7d1b18ee861
Mike Bryant <mikesname@gmail.com>
2021-03-05 13:35:57 +0000

    Allow no-op transformations for XSLT

--- modules/admin/app/services/transformation/SaxonXsltXmlTransformer.scala
+++ modules/admin/app/services/transformation/SaxonXsltXmlTransformer.scala
@@ -11,27 +11,28 @@ import org.xml.sax.SAXParseException
 case class SaxonXsltXmlTransformer() extends XsltXmlTransformer {
 
   def transform(input: String, mapping: String): String = {
+    if (mapping.trim.isEmpty) input else {
+      val writer: StringWriter = new StringWriter()
+      val mapSource: StreamSource = new StreamSource(new StringReader(mapping))
+      val inputSource: StreamSource = new StreamSource(new StringReader(input))
 
-    val writer: StringWriter = new StringWriter()
-    val mapSource: StreamSource = new StreamSource(new StringReader(mapping))
-    val inputSource: StreamSource = new StreamSource(new StringReader(input))
+      val processor = new Processor(false)
+      val compiler = processor.newXsltCompiler()
 
-    val processor = new Processor(false)
-    val compiler = processor.newXsltCompiler()
+      try {
+        val stylesheet = compiler.compile(mapSource)
+        val out = processor.newSerializer(writer)
+        out.setOutputProperty(Serializer.Property.OMIT_XML_DECLARATION, "yes")
+        out.setOutputProperty(Serializer.Property.INDENT, "yes")
 
-    try {
-      val stylesheet = compiler.compile(mapSource)
-      val out = processor.newSerializer(writer)
-      out.setOutputProperty(Serializer.Property.OMIT_XML_DECLARATION, "yes")
-      out.setOutputProperty(Serializer.Property.INDENT, "yes")
-
-      val transformer = stylesheet.load30()
-      transformer.transform(inputSource, out)
-      writer.toString
-    } catch {
-      case e: SaxonApiException => throw InvalidMappingError(e.getMessage)
-      case e: TransformerConfigurationException => throw InvalidMappingError(e.getMessage)
-      case e: SAXParseException => throw XmlTransformationError(e.getLineNumber, e.getColumnNumber, e.getMessage)
+        val transformer = stylesheet.load30()
+        transformer.transform(inputSource, out)
+        writer.toString
+      } catch {
+        case e: SaxonApiException => throw InvalidMappingError(e.getMessage)
+        case e: TransformerConfigurationException => throw InvalidMappingError(e.getMessage)
+        case e: SAXParseException => throw XmlTransformationError(e.getLineNumber, e.getColumnNumber, e.getMessage)
+      }
     }
   }
 }
