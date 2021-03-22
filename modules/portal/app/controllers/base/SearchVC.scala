9e8c826b8c6b710c2bc470ab0c80ca1c157f75c5
Mike Bryant <mikesname@gmail.com>
2021-03-22 16:44:02 +0000

    More Scala 2.13 compat fixes

--- modules/portal/app/controllers/base/SearchVC.scala
+++ modules/portal/app/controllers/base/SearchVC.scala
@@ -86,11 +86,11 @@ trait SearchVC {
     item match {
       case v: VirtualUnit => vcDescendantIds(item.id).map { seq =>
         if (seq.isEmpty) Map(ITEM_ID -> "__NO_VALID_ID__")
-        else Map(s"$ITEM_ID:(${seq.mkString(" ")}) OR $ANCESTOR_IDS:(${seq.mkString(" ")})" -> Unit)
+        else Map((s"$ITEM_ID:(${seq.mkString(" ")}) OR $ANCESTOR_IDS:(${seq.mkString(" ")})", ()))
       }
       // otherwise, for documentary units, we can just query
       // all ancestors
-      case d => immediate(Map(s"$ANCESTOR_IDS:${item.id}" -> Unit))
+      case d => immediate(Map((s"$ANCESTOR_IDS:${item.id}", ())))
     }
 
   protected def buildChildSearchFilter(item: Model): Future[Map[String,Any]] =
@@ -108,9 +108,9 @@ trait SearchVC {
       item match {
         case v: VirtualUnit =>
           val pq = v.includedUnits.map(_.id)
-          if (pq.isEmpty) Map(s"$PARENT_ID:${v.id}" -> Unit)
-          else Map(s"$PARENT_ID:${v.id} OR $ITEM_ID:(${pq.mkString(" ")})" -> Unit)
-        case d => Map(s"$PARENT_ID:${d.id}" -> Unit)
+          if (pq.isEmpty) Map((s"$PARENT_ID:${v.id}", ()))
+          else Map((s"$PARENT_ID:${v.id} OR $ITEM_ID:(${pq.mkString(" ")})", ()))
+        case d => Map((s"$PARENT_ID:${d.id}", ()))
       }
     }
 }
--- modules/portal/app/controllers/portal/Bookmarks.scala
+++ modules/portal/app/controllers/portal/Bookmarks.scala
@@ -147,10 +147,10 @@ case class Bookmarks @Inject()(
 
   private def buildFilter(v: VirtualUnit): Map[String, Any] = {
     val pq = v.includedUnits.map(_.id)
-    if (pq.isEmpty) Map(s"${SearchConstants.PARENT_ID}:${v.id}" -> Unit)
+    if (pq.isEmpty) Map((s"${SearchConstants.PARENT_ID}:${v.id}", ()))
     else {
       val q = s"${SearchConstants.PARENT_ID}:${v.id} OR ${SearchConstants.ITEM_ID}:(${pq.mkString(" ")})"
-      Map(q -> Unit)
+      Map((q, ()))
     }
   }
 
