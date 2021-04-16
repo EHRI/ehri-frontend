94688d4835e91b7bff7e4609f6b113ac48eab8aa
Mike Bryant <mikesname@gmail.com>
2021-04-16 19:13:30 +0100

    Put metadata tables in a scrollable box to prevent overflow issues

--- modules/admin/app/assets/js/datamanager/components/_modal-info.vue
+++ modules/admin/app/assets/js/datamanager/components/_modal-info.vue
@@ -15,22 +15,24 @@ export default {
 <template>
   <modal-window v-on:close="$emit('close')">
     <template v-slot:title>{{ decodeURI(fileInfo.meta.key) }}</template>
-    <h4>Attributes</h4>
-    <table class="info-table">
-      <tr>
-        <td>File size:</td>
-        <td>{{ humanFileSize(fileInfo.meta.size) }}</td>
-      </tr>
-      <tr>
-        <td>Last Modified:</td>
-        <td v-bind:title="fileInfo.meta.lastModified">{{ prettyDate(fileInfo.meta.lastModified) }}</td>
-      </tr>
-      <tr>
-        <td>E-Tag:</td>
-        <td v-bind:title="fileInfo.meta.eTag">{{fileInfo.meta.eTag}}</td>
-      </tr>
-    </table>
-    <template v-if="fileInfo.user">
+    <div class="file-meta">
+      <h4>Attributes</h4>
+      <table class="info-table">
+        <tr>
+          <td>File size:</td>
+          <td>{{ humanFileSize(fileInfo.meta.size) }}</td>
+        </tr>
+        <tr>
+          <td>Last Modified:</td>
+          <td v-bind:title="fileInfo.meta.lastModified">{{ prettyDate(fileInfo.meta.lastModified) }}</td>
+        </tr>
+        <tr>
+          <td>E-Tag:</td>
+          <td v-bind:title="fileInfo.meta.eTag">{{fileInfo.meta.eTag}}</td>
+        </tr>
+      </table>
+    </div>
+    <div class="file-meta" v-if="fileInfo.user">
       <h4>Metadata</h4>
       <table class="info-table">
         <tr v-for="(value, key) in fileInfo.user">
@@ -38,10 +40,10 @@ export default {
           <td>{{ value }}</td>
         </tr>
       </table>
-    </template>
-    <template v-if="fileInfo.versions && fileInfo.versions.length > 1">
+    </div>
+    <div class="file-user-meta" v-if="fileInfo.versions && fileInfo.versions.length > 1">
       <h4>Version History</h4>
-      <table class="version-history-list table table-striped table-sm table-bordered">
+      <table class="version-history-list info-table">
         <tr>
           <th>ID</th>
           <th>Created</th>
@@ -53,7 +55,7 @@ export default {
           <td>{{ humanFileSize(version.size) }}</td>
         </tr>
       </table>
-    </template>
+    </div>
     <template v-slot:footer>
       <button v-on:click="$emit('close')" type="button" class="btn btn-default">
         Close
