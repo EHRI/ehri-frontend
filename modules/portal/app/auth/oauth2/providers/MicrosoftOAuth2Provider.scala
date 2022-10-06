fb6559afd58f8623b85d3efdcb655500f1758386
Mike Bryant <mikesname@gmail.com>
2022-10-06 11:01:39 +0100

    Tweak Microsoft OIDC for updated info URL and data format:
    Now using: https://graph.microsoft.com/oidc/userinfo

--- modules/portal/app/auth/oauth2/providers/MicrosoftOAuth2Provider.scala
+++ modules/portal/app/auth/oauth2/providers/MicrosoftOAuth2Provider.scala
@@ -18,25 +18,17 @@ case class MicrosoftOAuth2Provider(config: play.api.Configuration) extends OAuth
       val json: JsValue = Json.parse(data)
       logger.debug(s"Microsoft user info $json")
       for {
-        guid <- (json \ "id").asOpt[String]
-        // NB: Microsoft doesn't make it easy to get emails: the "mail" property
-        // ostensibly contains the email but might well be empty because the user
-        // or organisation has to set this explicitly. OTOH the "userPrincipalName"
-        // field is always populated with an email-format address but it *might* not
-        // be exactly what the user knows as their email address - at least it should
-        // be an alias however. In my case, with a KCL-email address run by MS the
-        // "userPrincipalName" field gives my correct email address whereas "mail"
-        // is null.
-        // Also note: for registration and login via OAuth2 the user's email is
-        // pre-verified so it's not vital they can receive mail through it.
-        email <- (json \ "mail").asOpt[String].filter(_.nonEmpty)
-          .orElse((json \ "userPrincipalName").asOpt[String])
-        name <- (json \ "displayName").asOpt[String]
+        // https://learn.microsoft.com/en-us/azure/active-directory/develop/userinfo#calling-the-api
+        // URL is: https://graph.microsoft.com/oidc/userinfo/
+        guid <- (json \ "sub").asOpt[String]
+        email <- (json \ "email").asOpt[String]
+        name <- (json \ "name").asOpt[String]
+        picture = (json \ "picture").asOpt[String]
       } yield UserData(
         providerId = guid,
         email = email,
         name = name,
-        imageUrl = None
+        imageUrl = picture
       )
     } catch {
       case e: JsonParseException =>
