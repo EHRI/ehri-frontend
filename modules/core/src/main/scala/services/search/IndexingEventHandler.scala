f35ac5de5938795f49ce5536eeae412638764548
Mike Bryant <mikesname@gmail.com>
2023-03-28 15:13:10 +0100

    Attempts to make bulk operations more resilient to timeouts

--- modules/core/src/main/scala/services/search/IndexingEventHandler.scala
+++ modules/core/src/main/scala/services/search/IndexingEventHandler.scala
@@ -1,15 +1,17 @@
 package services.search
 
 import akka.actor.ActorRef
-import play.api.Logger
-import services.data.EventForwarder.{Create, Update, Delete}
+import play.api.{Configuration, Logger}
+import services.data.EventForwarder.{Create, Delete, Update}
 import services.data.EventHandler
 
 import javax.inject.{Inject, Named}
+import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
 
 case class IndexingEventHandler @Inject()(
   searchIndexer: SearchIndexMediator,
+  config: Configuration,
   @Named("event-forwarder") forwarder: ActorRef
 )(implicit ec: ExecutionContext) extends EventHandler {
 
@@ -19,8 +21,12 @@ case class IndexingEventHandler @Inject()(
   // to the SearchIndexMediator update/delete handlers. Do this
   // asynchronously and log any failures...
 
-  import java.util.concurrent.TimeUnit
   import scala.concurrent.duration.Duration
+
+  private lazy val threshold = config.get[Int]("ehri.admin.bulkOperations.threshold")
+  private def timeoutMinutes(items: Int): Duration = {
+    if (items < threshold) 1.minute else config.get[Duration]("ehri.admin.bulkOperations.timeout")
+  }
 
   private def logFailure(ids: Seq[String], func: Seq[String] => Future[Unit]): Unit = func(ids).failed.foreach {
     t => logger.error(s"Indexing error", t)
@@ -40,7 +46,9 @@ case class IndexingEventHandler @Inject()(
   // after redirects because the item is still in the search index but not in
   // the database.
   def handleDelete(ids: String*): Unit = logFailure(ids, ids => Future.successful[Unit] {
-    forwarder ! Delete(ids)
-    concurrent.Await.result(searchIndexer.handle.clearIds(ids: _*), Duration(1, TimeUnit.MINUTES))
+    ids.grouped(threshold * 2).map { group =>
+      forwarder ! Delete(group)
+      concurrent.Await.result(searchIndexer.handle.clearIds(group: _*), timeoutMinutes(ids.size))
+    }
   })
 }
