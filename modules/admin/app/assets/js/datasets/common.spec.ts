7cac74eab3797af3a263ea966a41a99ad9b9ec75
Mike Bryant <mikesname@gmail.com>
2023-04-17 16:02:22 +0100

    Disable quoting on TSV parsing, to avoid mangling Excel format files
    This is a bit dangerous for encoding, since values cannot contain
    tabs (FIXME) but previously it would completely break mapping
    conversions.
    
    Ultimately we should use a safer format (like JSON) with fewer
    ambiguities, but until then use Excel-style nothing-is-quoted
    style.

--- modules/admin/app/assets/js/datasets/common.spec.ts
+++ modules/admin/app/assets/js/datasets/common.spec.ts
@@ -10,8 +10,26 @@ test("encodeTsv", () => {
   expect(tsv).toBe("header1\theader2\na\tb\nc\td");
 });
 
+test("encodeTsv with quotes", () => {
+  let data = [["header1", "header2"], ["a", "\"b\""], ["c", "d with \"quotes\""]];
+  let tsv = encodeTsv(data, 2);
+  expect(tsv).toBe("header1\theader2\na\t\"b\"\nc\td with \"quotes\"");
+});
+
+test("encodeTsv with tabs", () => {
+  let data = [["header1", "header2"], ["a", "b"], ["c", "d\te"]];
+  let tsv = encodeTsv(data, 2);
+  expect(tsv).toBe("header1\theader2\na\tb\nc\td\te");
+});
+
 test("decodeTsv", () => {
   let tsv = "header1\theader2\na\tb\nc\td\n";
   let data = decodeTsv(tsv, 2);
   expect(data).toStrictEqual([["header1", "header2"], ["a", "b"], ["c", "d"]])
+});
+
+test("decodeTsv with quotes", () => {
+  let tsv = "header1\theader2\na\t\"b\"\nc\td with \"quotes\"\n";
+  let data = decodeTsv(tsv, 2);
+  expect(data).toStrictEqual([["header1", "header2"], ["a", "\"b\""], ["c", "d with \"quotes\""]]);
 });
