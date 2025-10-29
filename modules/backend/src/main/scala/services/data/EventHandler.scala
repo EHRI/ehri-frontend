eeca01de60ae3a861be7a8eee9c73b91c516d4b5
Mike Bryant <mikesname@gmail.com>
2025-10-29 14:47:57 +0000

    Update the event system to pass item types along with IDs.
    Also update the SSE stream to include this info in an extra array

--- modules/backend/src/main/scala/services/data/EventHandler.scala
+++ modules/backend/src/main/scala/services/data/EventHandler.scala
@@ -1,11 +1,8 @@
 package services.data
 
 import models.EntityType
-import org.apache.pekko.actor.ActorRef
 
 trait EventHandler {
-  def subscribe(actorRef: ActorRef): Unit = ()
-  def unsubscribe(actorRef: ActorRef): Unit = ()
   def handleCreate(items: (EntityType.Value, String)*): Unit
   def handleUpdate(items: (EntityType.Value, String)*): Unit
   def handleDelete(items: (EntityType.Value, String)*): Unit
--- test/integration/admin/SSESpec.scala
+++ test/integration/admin/SSESpec.scala
@@ -10,7 +10,7 @@ import play.api.test.FakeRequest
 /**
  * Spec to test various page views operate as expected.
  */
-class UtilsSpec extends IntegrationTestRunner with FakeMultipartUpload {
+class SSESpec extends IntegrationTestRunner with FakeMultipartUpload {
 
   override def getConfig: Map[String, Any] =
     super.getConfig ++ Map("ehri.eventStream.keepAlive" -> "10 millis")
@@ -20,21 +20,6 @@ class UtilsSpec extends IntegrationTestRunner with FakeMultipartUpload {
     def client(implicit app: Application) = {
       implicit val mat: Materializer = app.injector.instanceOf[Materializer]
       StandaloneAhcWSClient()
-    }
-
-    "return a successful ping of the EHRI REST service and search engine" in new ITestApp {
-      val ping = FakeRequest(controllers.admin.routes.Utils.checkServices()).call()
-      status(ping) must equalTo(OK)
-      contentAsString(ping) must_== "ehri\tok\nsolr\tok"
-    }
-
-    "check user sync correctly" in new ITestApp {
-      val check = FakeRequest(controllers.admin.routes.Utils.checkUserSync()).call()
-      status(check) must equalTo(OK)
-      // User joeblogs exists in the account mocks but not the
-      // graph DB fixtures, so the sync check should (correctly)
-      // highlight this.
-      contentAsString(check) must contain("joeblogs")
     }
 
     "check event stream connects and sends keep-alive events" in new ITestServer() {
