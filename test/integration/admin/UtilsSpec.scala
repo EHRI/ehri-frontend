4378ee70ea8744264d7e273dc70dc9f9c4446fb2
Mike Bryant <mikesname@gmail.com>
2025-10-29 14:47:57 +0000

    Event stream replay support.
    Update the event system to pass on item types as well as IDs. These
    events are emitted via SSE and also stored in the DB to facilitate
    replay.

--- test/integration/admin/UtilsSpec.scala
+++ test/integration/admin/UtilsSpec.scala
@@ -1,10 +1,6 @@
 package integration.admin
 
 import helpers._
-import org.apache.pekko.stream.Materializer
-import org.apache.pekko.stream.scaladsl.Sink
-import play.api.Application
-import play.api.libs.ws.ahc.StandaloneAhcWSClient
 import play.api.test.FakeRequest
 
 /**
@@ -12,15 +8,7 @@ import play.api.test.FakeRequest
  */
 class UtilsSpec extends IntegrationTestRunner with FakeMultipartUpload {
 
-  override def getConfig: Map[String, Any] =
-    super.getConfig ++ Map("ehri.eventStream.keepAlive" -> "10 millis")
-
   "Utils" should {
-
-    def client(implicit app: Application) = {
-      implicit val mat: Materializer = app.injector.instanceOf[Materializer]
-      StandaloneAhcWSClient()
-    }
 
     "return a successful ping of the EHRI REST service and search engine" in new ITestApp {
       val ping = FakeRequest(controllers.admin.routes.Utils.checkServices()).call()
@@ -35,20 +23,6 @@ class UtilsSpec extends IntegrationTestRunner with FakeMultipartUpload {
       // graph DB fixtures, so the sync check should (correctly)
       // highlight this.
       contentAsString(check) must contain("joeblogs")
-    }
-
-
-    "check event stream connects and sends keep-alive events" in new ITestServer() {
-      val req = client.url(s"http://localhost:${this.port}${controllers.admin.routes.Utils.sse()}")
-        .withHttpHeaders("Accept" -> "text/event-stream")
-        .stream()
-
-      val resp = await(req)
-      resp.status must_== OK
-      resp.contentType must_== "text/event-stream"
-
-      val events: Seq[String] = await(resp.bodyAsSource.take(3).map(_.utf8String).runWith(Sink.seq))
-      events.head must contain("data:")
     }
   }
 }
