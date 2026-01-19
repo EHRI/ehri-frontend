6c08fbdb6992911245ade09e91ba4a9e7e5b7e24
Mike Bryant <mikesname@gmail.com>
2026-01-19 10:23:15 +0000

    Improvements to Solr error handling
    If the search engine is down for some reason, don't log thousands
    of error messages but assume we'll be notified another way.

--- modules/solr/src/main/scala/eu/ehri/project/search/solr/SolrServerError.scala
+++ modules/solr/src/main/scala/eu/ehri/project/search/solr/SolrServerError.scala
@@ -1,13 +1,20 @@
 package eu.ehri.project.search.solr
 
-import play.api.libs.json._
 import play.api.libs.functional.syntax._
+import play.api.libs.json._
 
+/**
+  * Representation of a JSON Solr error response, which contains an `error` object
+  * with `code` and `msg` fields describing a problem.
+  *
+  * @param code the status code, e.g. 400 for malformed parameters
+  * @param msg  a description of the error
+  */
 case class SolrServerError(code: Int, msg: String) extends RuntimeException(msg)
 
 private object SolrServerError {
   implicit val _reads: Reads[SolrServerError] = (
-    (__ \ "error"\ "code").read[Int] and
+    (__ \ "error" \ "code").read[Int] and
     (__ \ "error" \ "msg").read[String]
   )(SolrServerError.apply _)
 }
